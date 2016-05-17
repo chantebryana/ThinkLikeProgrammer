56470e4e967963d51183bfcf38d4266655d5bec4
chantebryana <chantebryana@gmail.com>
2016-05-17 14:35:49 -0500

    More tiny progress
    turn digit into array (last time) and add all of the individual components
    of array, then print the final result out.  I like it!  :-)

--- luhnChecksumP31.rb
+++ luhnChecksumP31.rb
@@ -7,3 +7,10 @@ digitArray = digit.to_s.chars.map(&:to_i)
 digitArrayLong = digit.to_s.each_char.to_a.map{|char| char.to_i}
 # chars returns array of characters in string.  it's shorthand for: str.each_char.to_a
 # map(&:to_i) is also shorthand for map{|char| char.to_i} The & calls to_proc on the object, and passes it as a block to the method.
+
+sum = 0
+digitArray.each do |a|
+  sum+=a
+end
+print sum.to_s + "\n"
+#put "print" method after the each loop so that I only get the sum of the whole array, not the sum as the function's iterating through the array.
