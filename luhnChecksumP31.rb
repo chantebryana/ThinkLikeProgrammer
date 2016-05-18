835ada76168339999578e03a89ceb2d36c034221
chantebryana <chantebryana@gmail.com>
2016-05-17 20:42:23 -0500

    Continuing mapping out problem
    play around with finding the odd index, and then manipulating it.  I
    explored a number of items which seemed complex and I didn't fully
    understand (although I KNOW I could try to learn!).  I think I'm settling
    in on the pseudocode at the end of today's commitment.  It seems like it
    would make sense.

--- luhnChecksumP31.rb
+++ luhnChecksumP31.rb
@@ -4,13 +4,24 @@ digitDouble = digit * 2
 print digitDouble.to_s + "\n"
 
 digitArray = digit.to_s.chars.map(&:to_i)
-digitArrayLong = digit.to_s.each_char.to_a.map{|char| char.to_i}
-# chars returns array of characters in string.  it's shorthand for: str.each_char.to_a
-# map(&:to_i) is also shorthand for map{|char| char.to_i} The & calls to_proc on the object, and passes it as a block to the method.
 
 sum = 0
 digitArray.each do |a|
   sum+=a
 end
 print sum.to_s + "\n"
-#put "print" method after the each loop so that I only get the sum of the whole array, not the sum as the function's iterating through the array.
+# I could also use "inject"
+
+#this may be going about the problem the wrong way:
+digitArray.values_at(* digitArray.each_index.select {|i| i.odd?})
+
+#pseudocode:
+digitArray.map do 
+  if index % 2 == 1
+    element * 2
+	  if result > 9
+	    separate the numbers out and add them together
+	end
+  end
+end 
+
