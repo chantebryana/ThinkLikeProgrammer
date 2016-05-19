e96db02ba23cc229633e62117f642c59453c415d
chantebryana <chantebryana@gmail.com>
2016-05-19 12:28:41 -0500

    cleaning up

--- luhnChecksumP31.rb
+++ luhnChecksumP31.rb
@@ -10,27 +10,19 @@ digitArray.each_with_index do |value, index|
   valueArray = []
   valueSum = 0
   masterArray = []
-#  masterSum = 0
 # begin interal loops
   if index % 2 == 0
     masterArray << value
-#    print "even indeces: " + value.to_s + "\n"
   elsif index % 2 == 1
     valueDouble = value * 2
-#    print "odd indeces: " + value.to_s + "\n"
-#	print "valueDouble: " + valueDouble.to_s + "\n"
 	if valueDouble > 9
 	  valueArray = valueDouble.to_s.chars.map(&:to_i)
 	    valueArray.each do |a| 
 	      valueSum += a 
         end
-#	  print "valueSum: " + valueSum.to_s + "\n"
 	  masterArray << valueSum 
 	end
   end 
-#  masterArray.map do |value| 
-#    print "masterArray: " + value.to_s + ", "
-#  end 
   masterArray.each do |value| 
     $masterSum += value
   end 
@@ -42,14 +34,3 @@ if $masterSum % 10 == 0
 else 
   print "Dope! Data failed. \n"
 end
-
-=begin
-sum = 0
-digitArray.each do |a|
-  sum+=a
-end
-print "sum.to_s: " + sum.to_s + "\n"
-
-digitDouble = digit * 2
-print "digitDouble: " + digitDouble.to_s + "\n"
-=end
