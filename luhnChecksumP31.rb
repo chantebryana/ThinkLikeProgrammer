4240db14e69b3a033b35ed2208c1125216e53a14
chantebryana <chantebryana@gmail.com>
2016-05-18 20:33:56 -0500

    Successfully iterated over index of number array
    .each_with_index saved the day!  Now I need to flesh out the loop so that
    % 2 == 1 multiplies to value by 2 (and then saves that into a new variable
    % and sums the individual digits if the number's greater than 9), and then
    % sums the whole mess together and does the divisible by 10 check.

--- luhnChecksumP31.rb
+++ luhnChecksumP31.rb
@@ -4,6 +4,7 @@ digitDouble = digit * 2
 print digitDouble.to_s + "\n"
 
 digitArray = digit.to_s.chars.map(&:to_i)
+digitArray
 
 sum = 0
 digitArray.each do |a|
@@ -12,16 +13,9 @@ end
 print sum.to_s + "\n"
 # I could also use "inject"
 
-#this may be going about the problem the wrong way:
-digitArray.values_at(* digitArray.each_index.select {|i| i.odd?})
-
-#pseudocode:
-digitArray.map do 
+digitArray.each_with_index do |value, index|
   if index % 2 == 1
-    element * 2
-	  if result > 9
-	    separate the numbers out and add them together
-	end
-  end
-end 
+    print value.to_s + ", "
+  end 
+end
 
