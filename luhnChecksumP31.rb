df10de2fdd3c9a6e07e8c0252ba16f1e90215ff9
chantebryana <chantebryana@gmail.com>
2016-05-18 20:49:29 -0500

    Fleshed out loop: created branch if number > 9
    Includes some commented-out troubleshooting lines, for reference regarding
    how I made sure my lines of code work before getting too invested in the
    process.  I'm so smart!! :-)

--- luhnChecksumP31.rb
+++ luhnChecksumP31.rb
@@ -4,7 +4,6 @@ digitDouble = digit * 2
 print digitDouble.to_s + "\n"
 
 digitArray = digit.to_s.chars.map(&:to_i)
-digitArray
 
 sum = 0
 digitArray.each do |a|
@@ -14,8 +13,20 @@ print sum.to_s + "\n"
 # I could also use "inject"
 
 digitArray.each_with_index do |value, index|
+  valueDouble = 0
+  valueArray = []
+  valueSum = 0
   if index % 2 == 1
+    valueDouble = value * 2
     print value.to_s + ", "
+	if valueDouble > 9
+	  valueArray = valueDouble.to_s.chars.map(&:to_i)
+#valueArray.each {|i| print i.to_s + ", "}
+	  valueArray.each do |a| 
+	    valueSum += a 
+      end
+	  print valueSum.to_s
+	end
   end 
 end
 
