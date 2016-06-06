856bbf334e7b2d40cea6c2a8898bbfda158e67c3
chantebryana <chantebryana@gmail.com>
2016-06-06 12:57:57 -0500

    Attempting Mode Loop
    Create the protoplasyms of the loop that will cycle through the different
    modes: caps, down, symbol, and back again.  I still need to create the
    functions for each mode, or something similar.

--- decoderP61.rb
+++ decoderP61.rb
@@ -1,4 +1,5 @@
-print "User, enter integers separated by a comma and a space (eg, \"12, 31, 527\"), \nthen press Enter: "
+print "User, enter positive integers separated by a comma and a space \n"
+  + "(eg, \"12, 31, 527\"), then press Enter: "
 inputArray = gets.split(", ")
 
 #inputArray << gets.to_i 
@@ -16,7 +17,35 @@ downArray = %w[a b c d e f g h i j k l m n o p q r s t u v w x y z]
 message = "Hello World!"
 messageCode = [35, 27, 32, 39, 39, 42, 27, 14, 9, 50, 27, 42, 45, 39, 31, 27, 10]
 
+mode = 1 
 
+if mode == 1 
+  if integer % 27 == 0
+    mode = 2
+  elsif integer % 27 < 0
+    capsFunc 
+  else 
+    print "capsError!"
+  end 
+elsif mode == 2  
+  if integer % 27 == 0
+    mode = 3  
+  elsif integer % 27 < 0
+    downFunc 
+  else 
+    print "downError!"
+  end
+elsif mode == 3 
+  if integer % 9 == 0
+    mode = 1 
+  elsif integer % 9 < 0
+    symbolFunc 
+  else
+    print "symbolError!"
+  end 
+else 
+  print "modeError!"
+end
 
 capsArray.each_with_index do |valueCaps, indexCaps|
   inputArray.map do |valueInput|
