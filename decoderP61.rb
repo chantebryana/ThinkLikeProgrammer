7e8711372cbc7ea3cf75dfe550bb3f9fb5b0e23d
chantebryana <chantebryana@gmail.com>
2016-06-10 13:10:28 -0500

    Backtracking and simplifying
    my attempt to create a mega-loop utterly failed and expanded my scope too
    widely.  I've scaled back and am focusing on creating a coherent loop and
    mode definition and break for just the CAPS mode.  It works!  I still need
    to figure out how to expand this logic with multiple modes.  One thing at
    a time...

--- decoderP61.rb
+++ decoderP61.rb
@@ -17,62 +17,17 @@ downArray = %w[a b c d e f g h i j k l m n o p q r s t u v w x y z]
 message = "Hello World!"
 messageCode = [35, 27, 32, 39, 39, 42, 27, 14, 9, 50, 27, 42, 45, 39, 31, 27, 10]
 
-def capsFunc 
-  capsArray.each_with_index do |valueCaps, indexCaps|
-    inputArray.map do |valueInput|
-  	  if valueInput.to_i % 27 == indexCaps + 1 
-	    print valueCaps
-	  end 
-	end 
-  end 
-end 
-
-def downFunc 
-  downArray.each_with_index do |valueDown, indexDown|
-    inputArray.map do |valueInput|
-  	  if valueInput.to_i % 27 == indexDown + 1 
-	    print valueDown
-	  end 
-	end 
-  end 
-end 
-
-def symbolFunc 
-  capsArray.each_with_index do |valueSymbol, indexSymbol|
-    inputArray.map do |valueInput|
-  	  if valueInput.to_i % 27 == indexSymbol + 1 
-	    print valueSymbol
-	  end 
-	end 
-  end 
-end 
-
 mode = 1 
 
-if mode == 1 
-  if valueInput.to_i % 27 == 0
-    mode = 2
-  elsif valueInput.to_i % 27 > 0
-    capsFunc 
-  else 
-    print "capsError!"
-  end 
-elsif mode == 2  
-  if valueInput.to_i % 27 == 0
-    mode = 3  
-  elsif valueInput.to_i % 27 == indexDown + 1 
-    downFunc 
-  else 
-    print "downError!"
+capsArray.each_with_index do |valueCaps, indexCaps|
+  inputArray.map do |valueInput| 
+    if valueInput.to_i % 27 == indexCaps + 1 
+	  print valueCaps + "\n"
+	elsif valueInput.to_i % 27 == 0 
+	  mode = 2 
+	  break 
+	end 
   end
-elsif mode == 3 
-  if valueInput.to_i % 9 == 0
-    mode = 1 
-  elsif valueInput.to_i % 9 == indexSymbol + 1
-    symbolFunc 
-  else
-    print "symbolError!"
-  end 
-else 
-  print "modeError!"
 end
+
+print "mode: " + mode.to_s + "\n" 
