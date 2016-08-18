a2b1e6846136396d427c9caf22da02f55d403496
chantebryana <chantebryana@gmail.com>
2016-08-18 14:03:28 -0500

    attempt at nested loops: get a loooong line of single hashes.  :-)

--- TLaP_pg46.cpp
+++ TLaP_pg46.cpp
@@ -3,10 +3,14 @@ using std::cin;
 using std::cout;
 
 int main () {
-  int newLine = 5;
-  while (newLine > 0) {
-    cout << "#\n";
-    newLine--;
+  int newLine = 0;
+  while (newLine < 5) {
+    int hashRepeat = 5;
+    while (hashRepeat > 0) {
+      cout << "#\n";
+      hashRepeat--;
+    }
+    newLine++;
   }
   return 0;
 } 
