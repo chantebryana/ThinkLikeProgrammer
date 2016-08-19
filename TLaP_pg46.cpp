f72a80ae434ac2a6c7bc9c4c867d823c72909e66
chantebryana <chantebryana@gmail.com>
2016-08-19 12:43:21 -0500

    This minor edit prints out 6 lines instead of 5 (as I expected).

--- TLaP_pg46.cpp
+++ TLaP_pg46.cpp
@@ -5,7 +5,7 @@ using std::cout;
 //commenting
 int main () {
   int newLine = 0;
-  while (newLine < 5) {
+  while (newLine < 6) {
     int hashRepeat = 5;
     while (hashRepeat > 0) {
       cout << "#";
