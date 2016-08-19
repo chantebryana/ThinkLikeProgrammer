c300a3cff686ad2bc8bc5a63571fcc77f2c25bb2
chantebryana <chantebryana@gmail.com>
2016-08-19 12:50:52 -0500

    this minor change alters how many hashes are printed, as expected.

--- TLaP_pg46.cpp
+++ TLaP_pg46.cpp
@@ -2,16 +2,14 @@
 using std::cin;
 using std::cout;
 
-//commenting
 int main () {
   int newLine = 0;
   while (newLine < 5) {
-    int hashRepeat = 5;
+    int hashRepeat = 4;
     while (hashRepeat > 0) {
       cout << "#";
-      //hashRepeat--;
+      hashRepeat--;
     }
-    hashRepeat--;
     cout << "\n";
     newLine++;
   }
