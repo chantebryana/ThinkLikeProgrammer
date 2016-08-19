cf1906e69a83d946db682484311d1a8373bc85ec
chantebryana <chantebryana@gmail.com>
2016-08-19 14:15:17 -0500

    Function-ize
    Move some of my previous code into a separate function.  Not sure if it's laid out correctly / efficiently / readably, but hey, it works!

--- TLaP_pg46.cpp
+++ TLaP_pg46.cpp
@@ -2,16 +2,20 @@
 using std::cin;
 using std::cout;
 
+int hashFunc (int newLine) {
+  int hashRepeat = 0;
+  int comparison = 5;
+  while (comparison > 0) {
+    cout << "#";
+    hashRepeat++;
+    comparison = newLine - hashRepeat;
+  }
+}
+
 int main () {
   int newLine = 5;
   while (newLine > 0) {
-    int hashRepeat = 0;
-    int comparison = 5;
-    while (comparison > 0) {
-      cout << "#";
-      hashRepeat++;
-      comparison = newLine - hashRepeat;
-    }
+    hashFunc (newLine);
     cout << "\n";
     newLine--;
   }
