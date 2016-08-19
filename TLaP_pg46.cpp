b8715ede4403e402ac746d6ba6867c96469b10ca
chantebryana <chantebryana@gmail.com>
2016-08-19 13:56:26 -0500

    Incorporate Jim's suggestions and make it work!
    Next level-up would be to separate out the nested loops into separate functions.

--- TLaP_pg46.cpp
+++ TLaP_pg46.cpp
@@ -5,10 +5,12 @@ using std::cout;
 int main () {
   int newLine = 5;
   while (newLine > 0) {
-    int hashRepeat = 5;
-    while (hashRepeat > 0) {
+    int hashRepeat = 0;
+    int comparison = 5;
+    while (comparison > 0) {
       cout << "#";
-      hashRepeat--;
+      hashRepeat++;
+      comparison = newLine - hashRepeat;
     }
     cout << "\n";
     newLine--;
