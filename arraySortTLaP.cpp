3d8055ff8dc99d48feeddeaef89ee7a2799c3c10
chantebryana <chantebryana@gmail.com>
2016-09-08 15:26:32 -0500

    Minor fixes, aesthetic changes
    discover that ARRAY_SIZE = 10, but intArray only has 9 elements, which affects many areas.  Change intArray so it has 10 elements, and make adjustment to loop and other areas to make work as expected.  Also make minor aesthetic changes and add comments, flavor.

--- arraySortTLaP.cpp
+++ arraySortTLaP.cpp
@@ -9,12 +9,14 @@ int compareFunc(const void * voidA, const void * voidB) {
 }
 */
 int main () {
-  cout << "Hello Terminal\n";
-  const int ARRAY_SIZE = 10;
-  int intArray[ARRAY_SIZE] = {87, 28, 100, 78, 84, 98, 75, 81, 68};
-  for (int i = 0; i < ARRAY_SIZE - 1; i++) {
-    cout << "Unsorted array: " << intArray[i] << "\n";
+  cout << "Hello Terminal\n"; //playing around
+  cout << "Unsorted array: \n"; //print elements of unsorted array
+  const int ARRAY_SIZE = 10; //create constant for more generic code
+  int intArray[ARRAY_SIZE] = {87, 28, 100, 78, 84, 98, 75, 70, 81, 68}; //create unsorted array: what a mess!
+  for (int i = 0; i < ARRAY_SIZE; i++) {
+    cout << intArray[i] << "\n"; //print iterated elements of unsorted array
   }
+
 /*
   qsort(intArray, ARRAY_SIZE, sizeof(int), compareFunc);
   for (int i = 0; i <= ARRAY_SIZE; i++) {
