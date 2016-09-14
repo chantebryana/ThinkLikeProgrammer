1e628f8c02d1cf5bb8bdfcc50c88010bad98ee63
chantebryana <chantebryana@gmail.com>
2016-09-14 15:45:55 -0500

    Print out array: error
    Current code prints out index of array, not elements of array.  Silly me!

--- modeArrayDosP62.cpp
+++ modeArrayDosP62.cpp
@@ -3,7 +3,19 @@
 using std::cin;
 using std::cout;
 
+//define junk for later 
+int i;
+int result = 0;
+const int A_LENGTH = 19;
+
 int main () {
-	
+	//create unsorted array of survey responses 
+	int surveyResponses [A_LENGTH] = {2, 5, 9, 1, 5, 4, 7, 8, 6, 2, 10, 7, 4, 5, 6, 10, 9, 1, 1};
+	//print out unsorted responses 
+	cout << "Unsorted survey responses: ";
+	for (i = 0; i < A_LENGTH; i++) {
+		cout << i << " ";		
+	}
+	cout << "\n";
 	return 0;
 } 
