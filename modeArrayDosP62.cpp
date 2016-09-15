75a7f7a1165a812ab5bd7c35e833aa04bfbae873
chantebryana <chantebryana@gmail.com>
2016-09-15 15:52:23 -0500

    Minor changes: simplify array to make count func simpler
    Survey responses 1 to 5, not 1 to 10; other minor layout updates

--- modeArrayDosP62.cpp
+++ modeArrayDosP62.cpp
@@ -16,7 +16,8 @@ int compareFunc(const void *voidA, const void *voidB) {
 
 int main () {
 	//create unsorted array of survey responses 
-	int surveyResponses [A_LENGTH] = {2, 5, 9, 1, 5, 4, 7, 8, 6, 2, 10, 7, 4, 5, 6, 10, 9, 1, 1};
+	//int surveyResponses [A_LENGTH] = {2, 5, 9, 1, 5, 4, 7, 8, 6, 2, 10, 7, 4, 5, 6, 10, 9, 1, 1};
+	int surveyResponses [A_LENGTH] = {2, 5, 4, 1, 5, 4, 2, 3, 1, 2, 5, 2, 4, 5, 1, 5, 4, 1, 1}; //simlified: 1 to 5
 	//print out unsorted responses 
 	cout << "Unsorted survey responses: ";
 	for (i = 0; i < A_LENGTH; i++) {
@@ -26,7 +27,7 @@ int main () {
 	//sort the unsorted responses with qsort; use compareFunc defined above 
 	qsort(surveyResponses, A_LENGTH, sizeof(int), compareFunc);
 	//print out sorted responses!
-	cout << "Sorted survey responses: ";
+	cout << "  Sorted survey responses: ";
 	for (i = 0; i < A_LENGTH; i++) {
 		cout << surveyResponses[i] << " ";		
 	}
