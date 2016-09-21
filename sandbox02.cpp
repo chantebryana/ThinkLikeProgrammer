336f04309689520f51019a8a21188c14ba34c55c
chantebryana <chantebryana@gmail.com>
2016-09-21 13:08:04 -0500

    Successfully created "0" array ala the method mentioned on page 57 of TLaP

--- sandbox02.cpp
+++ sandbox02.cpp
@@ -1,9 +1,17 @@
 #include <iostream>
+//#define find_this and_replace_it_with_this ==> re using const int ARRAY_LENGTH as a number throughout the code
 using namespace std;
 
 //define those arrays!!
 const int ARRAY_LENGTH = 10;
-int tallyArr[ARRAY_LENGTH] = { };
+int tallyArr[ARRAY_LENGTH];
+
+//set values of tallyArr to 0
+void arrSet(int *arrayz) {
+	for (int i = 0; i < ARRAY_LENGTH; i++) {
+		arrayz[i] = 0;
+	}
+} 
 
 //iterate through array for printing out on screen
 void arrPrint(int *array) {
@@ -14,7 +22,8 @@ void arrPrint(int *array) {
 
 //call the arrPrint function in main 
 int main () {
-	arrPrint(&(*tallyArr));
+	arrSet(tallyArr);
+	arrPrint(tallyArr);
 	cout << "\n";
 	return 0;
 }
