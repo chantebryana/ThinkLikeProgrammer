06cd1cc1404ebdd547f82b8aeb0e1241931689dc
chantebryana <chantebryana@gmail.com>
2016-09-21 13:18:44 -0500

    playing around with index of array, not just its elements

--- sandbox02.cpp
+++ sandbox02.cpp
@@ -4,24 +4,28 @@ using namespace std;
 //define those arrays!!
 const int ARRAY_LENGTH = 10;
 int tallyArr[ARRAY_LENGTH];
+int i, n;
 
-//set values of tallyArr to 0
+//set values of tallyArr to 0, also print index of array 
 void arrSet(int *arrayz) {
-	for (int i = 0; i < ARRAY_LENGTH; i++) {
+	for (i = 0; i < ARRAY_LENGTH; i++) {
 		arrayz[i] = 0;
+		cout << i << " ";
 	}
 } 
 
 //iterate through array for printing out on screen
 void arrPrint(int *array) {
-	for (int n = 0; n < ARRAY_LENGTH; n++) {
+	for (n = 0; n < ARRAY_LENGTH; n++) {
 		cout << array[n] << " ";
 	}
 }
 
 //call the arrPrint function in main 
 int main () {
+	cout << "array indices: ";
 	arrSet(tallyArr);
+	cout << "\n" << "array values: ";
 	arrPrint(tallyArr);
 	cout << "\n";
 	return 0;
