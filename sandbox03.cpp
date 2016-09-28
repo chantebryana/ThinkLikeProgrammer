dc8920422dd1a403899a19953d730e92f8ea8456
chantebryana <chantebryana@gmail.com>
2016-09-28 14:59:26 -0500

    Setting the stage for qsorting: Exercise 3-1 of TLaP

--- sandbox03.cpp
+++ sandbox03.cpp
@@ -1,6 +1,16 @@
+#include <iostream>
+using namespace std;
 
+//define comparison function for qsort
+int compareFunc(const void *voidA, const void *voidB) {
+	int *intA = (int *)(voidA);
+	int *intB = (int *)(voidB);
+	return *intA - *intB;
+}
 
 int main () {
+	//sort the unsorted responses with qsort; use compareFunc defined above 
+	qsort(array, length, sizeof(int), compareFunc);
 	
 	return 0;
 }
