de6a42f017dbf47b396fc88dcde1641fae4d1246
chantebryana <chantebryana@gmail.com>
2016-09-27 19:59:27 -0500

    MODE MODE MODE

--- modeArrayTresP62.cpp
+++ modeArrayTresP62.cpp
@@ -9,6 +9,7 @@ const int A_LENGTH = 19;
 const int RESPONSE_LENGTH = 5;
 int ones, twos, threes, fours, fives = 0;
 int tallyArray [RESPONSE_LENGTH] = {};
+int modeCheck = 0;
 
 //create unsorted array of survey responses 
 //int surveyResponses [A_LENGTH] = {2, 5, 9, 1, 5, 4, 7, 8, 6, 2, 10, 7, 4, 5, 6, 10, 9, 1, 1};
@@ -21,14 +22,24 @@ int compareFunc(const void *voidA, const void *voidB) {
 	return *intA - *intB;
 }
 
-///*
 //how many of each number in survey responses?
 void eachNumCount (int *array, int length) {
 	for (i = 0; i < length; i++) {
 		tallyArray[(surveyResponses[i])-1]++;
 	}
 }
-//*/
+
+//function to determine mode!!
+int modeFunc (int *array, int length) {
+	int modeValue = 0;
+	for (i = 0; i < length; i++) {
+		if (array[i] > modeCheck) {
+			modeCheck = array[i];
+			modeValue = i + 1;
+		}
+	}
+	return modeValue;
+}
 
 int main () {
 	//print out unsorted responses 
@@ -54,5 +65,9 @@ int main () {
 	cout << "Threes: " << tallyArray[2] << "\n";
 	cout << " Fours: " << tallyArray[3] << "\n";
 	cout << " Fives: " << tallyArray[4] << "\n";
+	//compute the mode 
+	int modePrint = modeFunc(tallyArray, RESPONSE_LENGTH);
+	//print out the mode
+	cout << "Mode of survey responses: " << modePrint << "\n";
 	return 0;
 } 
