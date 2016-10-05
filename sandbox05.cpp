b62380d309c0aeedab502982131964e2eda4eca4
chantebryana <chantebryana@gmail.com>
2016-10-05 17:18:09 -0500

    Simplifying pointers: this version prints out the addresses of the variables, not the values

--- sandbox05.cpp
+++ sandbox05.cpp
@@ -5,17 +5,11 @@ using namespace std;
 int main () {
 	//declaring array, pointers
 	int a[2] = {2, 1};
-	int *p0 = a[0];
-	int *p1 = a[1];
-
-	//attempt at sorting func 
-	int sort(int *a) {
-		if (a[0] > a[1])
-			p0 = a[1];
-			p1 = a[0];
-		return &p0, &p1;
-	}
-	cout << sort(a) << "\n";
+	int *p0 = &a[0];
+	int *p1 = &a[1];
+	
+	//printing it out
+	cout << p0 << " " << p1 << " \n";
 	
 	return 0;
 }
