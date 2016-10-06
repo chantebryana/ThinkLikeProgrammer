be273b2f1c1c2c196e051640529af16495b87ecc
chantebryana <chantebryana@gmail.com>
2016-10-05 20:38:34 -0500

    Literal reassign of pointers to array elements

---
+++ sandbox06.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <cstdlib>
+using namespace std;
+
+
+
+int main () {
+	//declaring array, pointers
+	int a[2] = {2, 1};
+	int *p0 = &a[0]; //declare and define pointer 
+	int *p1 = &a[1]; //declare and define pointer 
+	
+	p0 = &a[1]; //literal reassign?
+	p1 = &a[0];
+	
+	//printing it out
+	cout << *p0 << " " << *p1 << " \n"; //prints "value pointed to by p0, p1"
+	
+	return 0;
+}
