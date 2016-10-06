9ffb27a98949f7513e4d9291f2c9210a63d040be
chantebryana <chantebryana@gmail.com>
2016-10-05 21:06:55 -0500

    MANUAL SORT FINALLY WORKED EVEN THOUGH IT'S WONKY JIM HELPED!!!!

--- sandbox06.cpp
+++ sandbox06.cpp
@@ -14,14 +14,22 @@ void sort(int *a, int *p0, int *p1) {
 int main () {
 	//declaring array, pointers
 	int a[2] = {2, 1};
-	int *p0 = &a[0]; //declare and define pointer 
-	int *p1 = &a[1]; //declare and define pointer 
+	int *pa = a;
+	int b[2] = {0, 0};
+	int *pb = b;
+	int *p0 = &pa[0]; //declare and define pointer 
+	int *p1 = &pa[1]; //declare and define pointer 
 	
 	//sorting???
-	if (a[0] > a[1]) {
-		p0 = &a[1];
-		p1 = &a[0];
+	int counter = 0;
+	if (pa[0] > pa[1]) {
+		b[0] = pa[1];
+		b[1] = pa[0];
 	}
+	pa = b;
+	
+	p0 = &pa[0]; //declare and define pointer 
+	p1 = &pa[1]; //declare and define pointer 
 	
 	//calling sorting function
 	//sort(*a, p0, p1);
