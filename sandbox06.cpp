9e9db5fc18dda833d6122404337833836af79711
chantebryana <chantebryana@gmail.com>
2016-10-05 20:51:05 -0500

    Creating a rudimentary sorting function: still requires some attention, but no compiling errors at least!

--- sandbox06.cpp
+++ sandbox06.cpp
@@ -2,16 +2,19 @@
 #include <cstdlib>
 using namespace std;
 
-
+int sort(int *a, int *p0, int *p1) {
+	if (a[0] > a[1]) {
+		p0 = &a[1];
+		p1 = &a[0];
+	}
+	return *p0, *p1;
+}
 
 int main () {
 	//declaring array, pointers
 	int a[2] = {2, 1};
 	int *p0 = &a[0]; //declare and define pointer 
 	int *p1 = &a[1]; //declare and define pointer 
-	
-	p0 = &a[1]; //literal reassign?
-	p1 = &a[0];
 	
 	//printing it out
 	cout << *p0 << " " << *p1 << " \n"; //prints "value pointed to by p0, p1"
