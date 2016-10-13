9ccb2ef6c650f5fcef6e09af1cd1bd560890b2a8
chantebryana <chantebryana@gmail.com>
2016-10-12 19:53:53 -0500

    I haven't tested / finalized this yet but I wanted to document what I typed off the top of my head.

--- sandbox06.cpp
+++ sandbox06.cpp
@@ -2,8 +2,15 @@
 #include <cstdlib>
 using namespace std;
 
+void switch (int* pA, *pB) {
+	int crane = pB;
+	pB = pA;
+	pA = crane;
+}
 
 int main () {
+	int numA = 2; 
+	int numB = 1;
 	
 	return 0;
 }
