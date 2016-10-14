8b89547250bb7e5e11113cb119544f93d4836db6
chantebryana <chantebryana@gmail.com>
2016-10-13 19:32:52 -0500

    including the cout within the function made the variables print out in the expected order.  But this is drifting further away from Jim's assignment, not closer to it.

--- sandbox06.cpp
+++ sandbox06.cpp
@@ -13,12 +13,13 @@ void switchFunc (int pA, int pB) {
 	int crane = pB;
 	pB = pA;
 	pA = crane;
+	cout << pA << ", " << pB << "\n";
 }
 
 int main () {
 	int numA = 2; 
 	int numB = 1;
 	switchFunc(numA, numB);
-	cout << numA << ", " << numB << "\n";
+//	cout << numA << ", " << numB << "\n";
 	return 0;
 }
