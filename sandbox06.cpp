8d6733962a03112a45f99d786a406b3d5967bcd8
chantebryana <chantebryana@gmail.com>
2016-10-13 19:36:16 -0500

    Attempt to cout within the pointer version of switchFunc() -- see comments for error messages.

--- sandbox06.cpp
+++ sandbox06.cpp
@@ -1,25 +1,18 @@
 #include <iostream>
 #include <cstdlib>
 using namespace std;
-/*
+
 void switchFunc (int* pA, int* pB) {
 	int crane = *pB;
 	*pB = *pA;
 	*pA = crane;
-}
-*/
-
-void switchFunc (int pA, int pB) {
-	int crane = pB;
-	pB = pA;
-	pA = crane;
-	cout << pA << ", " << pB << "\n";
+	cout << *pA << ", " << *pB << "\n";
 }
 
 int main () {
 	int numA = 2; 
 	int numB = 1;
 	switchFunc(numA, numB);
-//	cout << numA << ", " << numB << "\n";
+	//cout << numA << ", " << numB << "\n";
 	return 0;
 }
