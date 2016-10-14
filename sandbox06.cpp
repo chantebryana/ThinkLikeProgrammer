deeee1bf0dd72e970943fea06d436aedb233340d
chantebryana <chantebryana@gmail.com>
2016-10-14 15:17:54 -0500

    doesn't like void pointers!

--- sandbox06.cpp
+++ sandbox06.cpp
@@ -2,11 +2,13 @@
 #include <cstdlib>
 using namespace std;
 
-void switchFunc (int* pA, int* pB) {
-	int crane = *pB;
-	*pB = *pA;
-	*pA = crane;
-	cout << *pA << ", " << *pB << "\n";
+void switchFunc (void* pA, void* pB) {
+	int* intA = (int *)pA;
+	int* intB = (int *)pB;
+	int crane = *intB;
+	*intB = *intA;
+	*intA = crane;
+	cout << *intA << ", " << *intB << "\n";
 }
 
 int main () {
