36e9cc1f8de6010e78a971c4c0da988c7a23279f
chantebryana <chantebryana@gmail.com>
2016-10-14 23:41:50 -0500

    playing with switchFunc and learning more about how arrays and pointers work in real life

--- sandbox06.cpp
+++ sandbox06.cpp
@@ -18,11 +18,19 @@ int * sortFunc(int *intArray, int arrayLength){
 int main () {
 	//declare and define array
 	int a[2] = {2, 1};
+	//call sortFunc() 
+	switchFunc(&a[0], &a[1]);
+	/*
+	notes on how arrays and pointers work
+	a 		//int*
+	a[0] 	//int b/c effectively dereferencing
+	a[1]	//int also; *(a+1)
+
+*/
 	//loop to print
 	for (int i = 0; i<2; i++) {
 		cout << a[i] << " ";
 	}
 	cout << "\n";
-	
 	return 0;
 }
