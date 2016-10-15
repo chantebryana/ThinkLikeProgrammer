44e7054f1a945dca62faeef21541a16df044e9a4
chantebryana <chantebryana@gmail.com>
2016-10-14 23:21:11 -0500

    Switching gears: trying to solve second half of Jim's assignment

--- sandbox06.cpp
+++ sandbox06.cpp
@@ -8,10 +8,21 @@ void switchFunc (int* pA, int* pB) {
 	*pA = crane;
 }
 
+int * sortFunc(int *intArray, int arrayLength){
+    // use switchFunc to sort the contents of intArray here
+	
+	// return the intArray for obscure reasons (to make it 'pass through' the function)
+	return intArray;
+}
+
 int main () {
-	int numA = 2; 
-	int numB = 1;
-	switchFunc(&numA, &numB);
-	cout << numA << ", " << numB << "\n";
+	//declare and define array
+	int a[2] = {2, 1};
+	//loop to print
+	for (int i = 0; i<2; i++) {
+		cout << a[i] << " ";
+	}
+	cout << "\n";
+	
 	return 0;
 }
