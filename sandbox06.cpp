34fe1286745fc87b3c92d27e4c0a4069a8f1b925
chantebryana <chantebryana@gmail.com>
2016-10-14 22:51:06 -0500

    Made it muthafucking work.  Compare this code to that of "Oh my, really doesn't like this" --> so close but so different.

--- sandbox06.cpp
+++ sandbox06.cpp
@@ -6,13 +6,16 @@ void switchFunc (int* pA, int* pB) {
 	int crane = *pB;
 	*pB = *pA;
 	*pA = crane;
-	cout << *pA << ", " << *pB << "\n";
+	//cout << *pA << ", " << *pB << "\n";
 }
 
 int main () {
 	int numA = 2; 
 	int numB = 1;
-	switchFunc((int*)numA, (int*)numB);
-	//cout << numA << ", " << numB << "\n";
+	int* pnA = &numA;
+	int* pnB = &numB;
+	switchFunc(pnA, pnB);
+//	switchFunc((int*)numA, (int*)numB);
+	cout << numA << ", " << numB << "\n";
 	return 0;
 }
