cca3ddbf6bffabe6868052765312aed40a156b57
chantebryana <chantebryana@gmail.com>
2016-11-01 22:22:14 -0500

    Removing unused code: the whole "smallest" function.

--- sandbox08.cpp
+++ sandbox08.cpp
@@ -27,26 +27,6 @@ int * sortFunc(int *intArray, int arrayLength){
 	return intArray;
 }
 
-int smallest(int* array, int arrLength) {
-	int compVar = 100; //this is a number larger than anything in the array
-	int indexTrack = 0; //this trazcks the index of the smallest value
-	for (int i = 0; i < arrLength; i++) { //i starting at 0 will actually depend on indexTally; 3 is the size of the array defined in main
-		if (compVar > array[i]) {
-			compVar = array[i];
-			indexTrack = i;
-		}
-	}
-	return indexTrack;
-}
-
-/*d
-int main() {
-	int a[3] = {1, 2, 3};
-	cout << a[smallest(a)] << "\n";
-	return 0;
-}
-*/
-
 int main () {
 	//define constants
 	int const ALENGTH = 7;
