dbc8baff0f6a045bab4e91cf3ab4190e16088bd9
chantebryana <chantebryana@gmail.com>
2016-11-03 16:31:39 -0500

    This totally doesn't work at all either.

--- sandbox09.cpp
+++ sandbox09.cpp
@@ -8,8 +8,13 @@ void print(int x) {
 	printf("%d\n", x);
 }
 
+func(print);
+
 int main () {
-	char y[] = "printing";
-	func(print);	
+	void func (void (*f)(int)) {
+		for (int counter = 0; counter < 5; counter++) {
+			(*f)(counter);
+		}
+	}
 	return 0;
 }
