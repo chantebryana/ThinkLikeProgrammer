da4fb7a1df5d128ca7c450a4e292178bfc4931fa
chantebryana <chantebryana@gmail.com>
2016-11-10 11:05:20 -0600

    basic pointer func: will it work??

--- sandbox10.cpp
+++ sandbox10.cpp
@@ -1,12 +1,20 @@
 #include <iostream>
 using namespace std;
 
+// adding func
+// http://www.cplusplus.com/forum/windows/32209/
 int add(int * array, int length){
 	int sum = 0;
 	for (int i = 0; i < length; i++) {
 		sum += array[i];
 	}
 	return sum;
+}
+
+// attempt at pointer func
+// https://en.wikipedia.org/wiki/Function_pointer
+int pFunc(int first, int second, int (*funcToCall)(int, int)) {
+	return (*funcToCall)(first, second);
 }
 
 int main () {
