e1e8bf4beafe56737fb997162378a4a73c0bdc9d
chantebryana <chantebryana@gmail.com>
2016-11-10 13:05:36 -0600

    SUCCESSFULLY CREATED SECONDARY FUNC THAT PASSED POINTER FUNC!  win win win

--- sandbox10.cpp
+++ sandbox10.cpp
@@ -11,17 +11,23 @@ int add(int * array, int length){
 	return sum;
 }
 
-// attempt at pointer func
+// pointer func
 // https://en.wikipedia.org/wiki/Function_pointer
 int pFunc(int * arrPoint, int length, int (*funcToCall)(int *, int)) {
 	return (*funcToCall)(arrPoint, length);
+}
+
+// attempt to create a secondary func that passes pointer func as argument
+void printFunc(int pointFunc) {
+	cout << pointFunc << "\n";
 }
 
 int main () {
 	const int ALENGTH = 4;
 	int a[ALENGTH] = {4, 3, 2, 1};
 	//attempt at pointer func...
-	int b = pFunc(a, ALENGTH, add);
-	cout << b << "\n";
+	//int b = pFunc(a, ALENGTH, add);
+	//cout << b << "\n";
+	printFunc(pFunc(a, ALENGTH, add));
 	return 0;
 }
