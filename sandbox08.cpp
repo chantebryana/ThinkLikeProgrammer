227f124f3b8ad7b822d54c6e811f37767eae026d
chantebryana <chantebryana@gmail.com>
2016-11-09 21:01:02 -0600

    reverting back to initial "pointer func" state: don't mess with sorting cstring array; rather, attempt to make more generic pointer function qsort-stand-in (ugh)

--- sandbox08.cpp
+++ sandbox08.cpp
@@ -25,11 +25,12 @@ int pointFunc(int * first, int * second, int (*funcToCall)(int *, int *)) {
 /*
 Bubble Sort Func: nested loops make sure the function compares and sorts the entire array; shorterVar removes comparing elements at the end that are already in their proper place; black boxes like compLogicFunc and switchFunc make the various pieces separate and more generalized; return intArray to pass the sorted array onwards.
 */ 
-int * sortFunc(int *intArray, int arrayLength, pointFunc(&intArray[i], &intArray[i+1], compLogicFunc)){
+//int (*compPointer)(int *, int *) = compLogicFunc;
+int * sortFunc(int *intArray, int arrayLength){
 	for (int h = 0; h < arrayLength-1; h++) {		
 		int shorterVar = arrayLength - 1; 
 		for (int i = 0; i < shorterVar; i++) {	
-			if (pointFunc) {
+			if (compLogicFunc (&intArray[i], &intArray[i+1])) {
 				switchFunc(&intArray[i], &intArray[i+1]);	
 			} 
 		} shorterVar --;
@@ -40,9 +41,9 @@ int * sortFunc(int *intArray, int arrayLength, pointFunc(&intArray[i], &intArray
 int main () {
 	//define constants, array
 	int const ALENGTH = 5;
-	char a[ALENGTH] = {'k', 'a', 'b', 'z', 'g'};
+	int a[ALENGTH] = {5, 4, 3, 2, 1};
 	//call sortFunc() 
-//	sortFunc(a, ALENGTH);
+	sortFunc(a, ALENGTH);
 	
 	//loop to print
 	for (int i = 0; i<ALENGTH; i++) {
