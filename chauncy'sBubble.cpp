9906bd1ba7558e6626cf029a7ee0e1f02a28e229
chantebryana <chantebryana@gmail.com>
2016-11-19 13:23:27 -0600

    COMMENTS!!!

--- chauncy'sBubble.cpp
+++ chauncy'sBubble.cpp
@@ -8,25 +8,27 @@ int compLogicFunc(int * intArrayA, int * intArrayB) {
 }
 
 int num[] = {1, 2, 3, 4, 5, 6, 7, 8, 9};
+//goal: to sort like this: [8, 6, 4, 2, 1, 3, 5, 7, 9]
 
-if (num[i] % 2 == 0) {
-	if (num[i+] % 2 == 0) {
-		if (num[i] > num[i+1]) {
+//1 is "true", 0 is "false"
+if (num[i] % 2 == 0) {					//if first element's even
+	if (num[i+1] % 2 == 0) {			//if second element's even
+		if (num[i] > num[i+1]) {		//if first element's larger than second
 			return 1;
-		} else {
+		} else {								//if first element's smaller than second
 			return 0;
 		}
-	} else if (num[i+1] % 2 == 1) {
+	} else if (num[i+1] % 2 == 1) {	//if second element's odd
 		return 0;
 	}
-} else if (num[i] % 2 == 1) {
-	if (num[i+1] % 2 == 1) {
-		if (num[i] > num[i+1]) {
+} else if (num[i] % 2 == 1) {			//if first element's odd
+	if (num[i+1] % 2 == 1) {			//if second element's odd
+		if (num[i] > num[i+1]) {		//if first element's larger than second
 			return 1;
-		} else {
+		} else {								//if first element's smaller than second
 			return 0;
 		}
-	} else if (num[i+1] % 2 == 0) {
+	} else if (num[i+1] % 2 == 0) {	//if second element's even
 		return 1;
 	}
 }
