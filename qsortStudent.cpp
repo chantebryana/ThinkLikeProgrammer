f6854fae53fea7f3aaa17f0a5d7c94987684860f
chantebryana <chantebryana@gmail.com>
2016-11-24 20:39:05 -0600

    remove string portion of student, studentArray: core doesn't dump and numbers sort properly.

--- qsortStudent.cpp
+++ qsortStudent.cpp
@@ -12,22 +12,21 @@ int main() {
 	struct student {
 		int grade; 
 		int studentID;
-		string name;
 	};
 	// declare and define array size, plus multi-dimensional student array
 	// use struct student for student array data type
 	const int ARRAY_SIZE = 10;
 	student studentArray[ARRAY_SIZE] = {
-		 {87, 10001, "Fred"},
-		 {28, 10002, "Tom"},
-		 {100, 10003, "Alistair"},
-		 {78, 10004, "Sasha"},
-		 {84, 10005, "Erin"},
-		 {98, 10006, "Belinda"},
-		 {75, 10007, "Leslie"},
-		 {70, 10008, "Candy"},
-		 {81, 10009, "Aretha"},
-		 {68, 10010, "Veronica"}
+		 {87, 10001},
+		 {28, 10002},
+		 {100, 10003},
+		 {78, 10004},
+		 {84, 10005},
+		 {98, 10006},
+		 {75, 10007},
+		 {70, 10008},
+		 {81, 10009},
+		 {68, 10010}
 	};
 
 	qsort(studentArray, ARRAY_SIZE, sizeof(student), compare);
