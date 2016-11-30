b13f904c813ef9d55cfdffcffaf5a6e5570b0769
chantebryana <chantebryana@gmail.com>
2016-11-30 11:59:47 -0600

    successfully print the array as a for loop; unfilled boxes of array print strange characters on terminal

--- pointString.cpp
+++ pointString.cpp
@@ -8,11 +8,19 @@ void characterAt(char * array, int * indexToFind) {
 	cout << array[* indexToFind] << "\n";
 }
 
+//print the array!
+void printIt(char * value, int length) {
+	for (int i = 0; i < length; i++) {
+		cout << value[i] << " ";
+	}
+	cout << ".\n";
+}
+
 int main() {
 	char userString[20];		// http://www.cplusplus.com/forum/beginner/76615/
 	cout << "Enter a single-word string, then press ENTER: ";
 	cin >> userString;
-	cout << "The string you entered is " << userString << "\n";
+	printIt(userString, 20);
 	int const aLength = 5;
 	char a[aLength] = {'a', 'b', 'c', 'd', 'e'};
 	int positionIndex = 3;
