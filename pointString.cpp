fcfbbc978a5c48b05a992aee46ddf4d3c33e0dab
chantebryana <chantebryana@gmail.com>
2016-11-30 11:39:01 -0600

    next step: utilizing user input.  still some work to do (only saving a single character to userString)

--- pointString.cpp
+++ pointString.cpp
@@ -9,10 +9,14 @@ void characterAt(char * array, int * indexToFind) {
 }
 
 int main() {
+	char userString;
+	cout << "Enter a single-word string, then press ENTER: ";
+	cin >> userString;
+	cout << "The string you entered is " << userString << "\n";
 	int const aLength = 5;
 	char a[aLength] = {'a', 'b', 'c', 'd', 'e'};
 	int positionIndex = 3;
 	int * positionPointer = &positionIndex;
-	characterAt(a, positionPointer);
+//	characterAt(a, positionPointer);
 	return 0;
 }
