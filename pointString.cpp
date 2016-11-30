ebc0713f938cd70b00fb59febab5bfdfdba8551a
chantebryana <chantebryana@gmail.com>
2016-11-30 07:04:07 -0600

    very initial babysteps to solve TLAP problem in chapter 4 (p 111 of PDF; p 91 of document).  Problems with pointer conversions, of course.

---
+++ pointString.cpp
@@ -0,0 +1,18 @@
+// Variable-Length String problem, TLAP Ch4
+#include <cstdlib>
+#include <iostream>
+using namespace std;
+
+//take a string and a number and return the character at the position in the string
+int characterAt(int * array, int * indexToFind) {
+	cout << array[indexToFind] << "\n";
+}
+
+int main() {
+	int const aLength = 5;
+	int a[aLength] = {1, 2, 3, 4, 5};
+	int positionIndex = 3;
+	int * positionPointer = positionIndex;
+	characterAt(a, positionPointer);
+	return 0;
+}
