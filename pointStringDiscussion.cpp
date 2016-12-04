982b10ef2a7c75b1452c4a8b5ffd5c8681d08a0c
chantebryana <chantebryana@gmail.com>
2016-12-03 18:48:11 -0600

    cleaning up commentary and making code reflect changes on pp 97-98 of TLAP

--- pointStringDiscussion.cpp
+++ pointStringDiscussion.cpp
@@ -3,21 +3,24 @@
 // test diagram / example: take word "test" and append with "!" 
 // [t, e, s, t, 0] to [t, e, s, t, !, 0]
 
-void append(arrayString& s, char c) {				// pass arguments of full word (address of) s array ("test") plus added character c ("!")
-	int oldLength = 0;
-	while (s[oldLength] != 0) {						// ie, till the last element of array, since last element will be null character (0)
-		oldLength++;										// makes oldLength the same length (minus 1) as your dynamically-created array
+int length(arrayString s) {
+	int count = 0;
+	while (s[count] != 0) {
+		count ++;
 	}
-	arrayString newS = new char[oldLength + 2];	// newS is new variable, which I think kind of works like crane (from bubble sort) 
-																// arrayString already defined as typedef char * 
-																// new char array points to dynamically-allocated heap memory 
-																// (of length 1 byte * oldLength + 2 I think)
+	return count;
+}
+
+void append(arrayString& s, char c) {
+	int oldLength = 
+	arrayString newS = new char[oldLength + 2];
+
 	for (int i = 0; i < oldLength; i++) {
-		newS[i] = s[i];									// assigns defintions of newS elements to be equivelant as elements of s array
+		newS[i] = s[i];
 	}
-	newS[oldLength] = c;									// in the case of "test": element at newS[4] = new character "!" 
-	newS[oldLength + 1] = 0;							// in the case of "test!": element at newW[5] = 0 or null
-	delete[] s;												// deallocate the memory location pointed to by s variable
-	s = newS;												// redefine variable s to equal array newS
+	newS[oldLength] = c;
+	newS[oldLength + 1] = 0;
+	delete[] s;	
+	s = newS;
 }
 
