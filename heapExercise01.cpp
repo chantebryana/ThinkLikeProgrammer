ec7f94a8d17eea8a794f47e91cda8fb68734e291
chantebryana <chantebryana@gmail.com>
2016-12-06 16:09:59 -0600

    cleaning up and adding commentary

--- heapExercise01.cpp
+++ heapExercise01.cpp
@@ -6,12 +6,14 @@ using std::string;
 using std::cin;
 using std::cout;
 
-
+// generic printing-of-an-array function
 void printFunc (char * output, int sLgth) {
 	for(int j = 0; j < sLgth; j++) {
 		cout << output[j];
 	}
 }
+
+// create a new array "output" to print only the subset of chars from original "array"
 void substring (char * array, int sPos, int sLgth) {
 	char output[sLgth];
 	int count = 0;
@@ -22,6 +24,7 @@ void substring (char * array, int sPos, int sLgth) {
 	printFunc(output, sLgth);
 }
 
+// literal reassignment func for testing purposes
 void substringLiteral(char * array, int sLgth) {
 	char output[sLgth];
 	output[0] = array[2];
@@ -34,10 +37,9 @@ void substringLiteral(char * array, int sLgth) {
 int main() {
 	int const ALENGTH = 7;
 	char a[ALENGTH] = {'a', 'b', 'c', 'd', 'e', 'f', 'g'};
-	int stringPosition = 2;
-	int stringLength = 4;
+	int stringPosition = 2;		// where to start at the char array
+	int stringLength = 4;		// how long to keep looking for
 	substring(a, stringPosition, stringLength);
-	//substringLiteral(a, stringLength);
 	cout << "\n";
 
 	return 0;
