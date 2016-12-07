ede078b1456f7c7c72f85170f86b04d526bd32fc
chantebryana <chantebryana@gmail.com>
2016-12-07 17:50:52 -0600

    cleaning up, adding more commentary

--- heapExercise01.cpp
+++ heapExercise01.cpp
@@ -6,13 +6,6 @@ using std::string;
 using std::cin;
 using std::cout;
 
-// generic printing-of-an-array function
-void printFunc (char * output, int sLgth) {
-	for(int j = 0; j < sLgth; j++) {
-		cout << output[j];
-	}
-}
-
 // create a new heap array "output" to print only the subset of chars from original "array"
 char * substring (char * array, int sPos, int sLgth) {
 	char * pArray = new char[sLgth];	// missing several potential error checks
@@ -22,9 +15,6 @@ char * substring (char * array, int sPos, int sLgth) {
 		count ++;
 	}
 	return pArray;
-
-//	printFunc(pArray, sLgth); // is this kosher?
-//	delete[] pArray; 	// does this properly free heap memory?
 }
 
 int main() {
@@ -32,7 +22,7 @@ int main() {
 	char a[ALENGTH] = {'a', 'b', 'c', 'd', 'e', 'f', 'g'};
 	int stringPosition = 2;		// where to start at the char array
 	int stringLength = 4;		// how long to keep looking for
-	char * output = substring(a, stringPosition, stringLength);
+	char * output = substring(a, stringPosition, stringLength);	// important assignment to maintain heap memory and allow me to deallocate it later.
 	cout << output << "\n";
 	delete[] output;
 
