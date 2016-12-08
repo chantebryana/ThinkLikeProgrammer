0078708fd819cc44bad54844233c8cdd73b2daef
chantebryana <chantebryana@gmail.com>
2016-12-08 13:09:05 -0600

    still nine error's: switched target to only have 1 letter to compare and still no change

--- heapExercise02.cpp
+++ heapExercise02.cpp
@@ -8,7 +8,7 @@ using std::cout;
 
 int findSame(char * source, char * targ) {
 	for(int i = 0; i < 9; i++) {
-		for(int j = 0; j < 2; j++) {
+		for(int j = 0; j < 1; j++) {
 			if( source[i] == targ[j] ) {
 				return 1;
 			} else {
@@ -31,7 +31,7 @@ void print(char * source, char * targ) {
 
 int main () {
 	char a[9] = {'a', 'b', 'c', 'd', 'b', 'c', 'e', 'c', 'b'};
-	char target[2] = {'b', 'c'};
+	char target[1] = {'b'};
 	char replace[3] = {'z', 'z', 'z'};
 	print(a, target);
 	return 0;
