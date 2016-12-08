ae6594be6b13e051668bfcc42b5523d5e6bd17c0
chantebryana <chantebryana@gmail.com>
2016-12-08 12:56:38 -0600

    derp derp: make the equals operator in findSame the right one: use check "==" not reassignment.  This code also doesn't throw any errors during compile time, but what's my next step?

--- heapExercise02.cpp
+++ heapExercise02.cpp
@@ -9,7 +9,7 @@ using std::cout;
 int findSame(char * source, char * targ) {
 	for(int i = 0; i < 9; i++) {
 		for(int j = 0; j < 2; j++) {
-			if( source[i] = targ[i] ) {
+			if( source[i] == targ[i] ) {
 				return 1;
 			} else {
 				return 0;
@@ -17,8 +17,6 @@ int findSame(char * source, char * targ) {
 		}
 	}
 }
-
-
 
 int main () {
 	char a[9] = {'a', 'b', 'c', 'd', 'b', 'c', 'e', 'c', 'b'};
