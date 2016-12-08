9787d3a1a805cdffc516a50ee6ffd059b8c48171
chantebryana <chantebryana@gmail.com>
2016-12-08 12:49:26 -0600

    breaking down problem 4.3 bit by bit.  Wrestling with replacing multiple elements of array with multiple more

---
+++ heapExercise02.cpp
@@ -0,0 +1,28 @@
+// TLAP chapter 4.3 exercise: proto
+
+#include <iostream>
+#include <cstdlib>
+using std::string;
+using std::cin;
+using std::cout;
+
+int findSame(char * source, char * targ) {
+	for(int i = 0; i < 9; i++) {
+		for(int j = 0; j < 2; j++) {
+			if( source[i] = targ[i] ) {
+				return 1;
+			} else {
+				return 0;
+			}
+		}
+	}
+}
+
+
+
+int main () {
+	char a[9] = {'a', 'b', 'c', 'd', 'b', 'c', 'e', 'c', 'b'};
+	char target[2] = {'b', 'c'};
+	char replace[3] = {'z', 'z', 'z'};
+	return 0;
+}
