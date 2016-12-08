dd3fc8ac90f7585b9be09e76e52f09f0979e157b
chantebryana <chantebryana@gmail.com>
2016-12-08 13:07:02 -0600

    fixed index for targ in findSame: was "i" and now it's "j".  Still getting 9 printed out "error"s.

--- heapExercise02.cpp
+++ heapExercise02.cpp
@@ -9,7 +9,7 @@ using std::cout;
 int findSame(char * source, char * targ) {
 	for(int i = 0; i < 9; i++) {
 		for(int j = 0; j < 2; j++) {
-			if( source[i] == targ[i] ) {
+			if( source[i] == targ[j] ) {
 				return 1;
 			} else {
 				return 0;
