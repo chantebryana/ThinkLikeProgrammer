8e73a0583fa7f91db0beabf50f7875cf7598e782
chantebryana <chantebryana@gmail.com>
2016-12-19 15:46:01 -0600

    no commit errors, but didn't print out the results I expected.  Used mishmash of lumping existing "literal assignment" code into a literal assignment function, and also the memory cleanup used in heapExercise01.cpp

--- heap02SandboxB.cpp
+++ heap02SandboxB.cpp
@@ -6,36 +6,29 @@ using std::string;
 using std::cin;
 using std::cout;
 
-/*
+
 // to begin with, i = 1 for newString[i]
-void replaceFunc(char oldString, char repl) {
-	char * newString[] = new char[6];
-	newString[0] = oldString[0]; 
-	for (int i = 1; i < 4; i++) {
-		for (int j = 0; j < 3; j++) {
-			newString[i] = repl[j];
-			}
+char * replaceFunc(char * oldString, char * repl) {
+	char * pA = new char[5];
+	pA[0] = oldString[0];
+		for (int i = 1; i < 4; i++) {
+			pA[i+1] = repl[i];
 		}
+	pA[4] = oldString[3];
+	return pA;
 	}
-
-	cout << newString << "\n";
-	delete[] newString;
-}
-*/
-
-void print(char * a) {
-	for (int i = 0; i < 5; i++) {
-		cout << a[i] << " ";
-	}
-	cout << "\n";
-}
 
 int main () {
 
 	char string[] = "abcd";
 	char target[] = "bc";
 	char replace[] = "xyz";
+	
+	char * output = replaceFunc(string, replace);
+	cout << output  << "\n";
+	delete[] output;
 
+/*
 	// based off of append code p 93
 	char * pA = new char[5];
 	cout << pA << "\n";
@@ -50,6 +43,6 @@ int main () {
 	
 	pA[4] = string[3];
 	cout << pA << "\n";
-
+*/
 	return 0;
 }
