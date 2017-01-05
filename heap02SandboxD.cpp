6b64c30b033b91e6b14fc5b7a3793e1e153076ed
chantebryana <chantebryana@gmail.com>
2017-01-05 11:48:48 -0600

    add comments

--- heap02SandboxD.cpp
+++ heap02SandboxD.cpp
@@ -3,6 +3,7 @@
 using std::string;
 using std::cout;
 
+// find length of baseString, for use in determining where to insert various characters to new memory allocation
 int findBaseLength(char * stringBase) {
 	int stringBaseLength = 0;
 	while (stringBase[stringBaseLength] !=0) {
@@ -11,6 +12,7 @@ int findBaseLength(char * stringBase) {
 	return stringBaseLength;
 }
 
+// find length of insertString, for use in determining where to insert various characters to new memory allocation
 int findInsertLength(char * stringInsert) {
 	int stringInsertLength = 0;
 	while (stringInsert[stringInsertLength] !=0) {
@@ -23,11 +25,15 @@ int findInsertLength(char * stringInsert) {
 // take insertString, and stuff it into baseString at the location of insertIndex (10).  Point to original char variables for the function, and create a new heap pointer to copy the chars over to a new allocation of larger memory.  Return heap pointer, which will be copied over to resultString in Main.  consider making a new black box for insertion of new string?
 
 char * doInsert(char * stringBase, int indexInsert, char * stringInsert) {
+	// assign vars to both string length functions
 	int stringBaseLength = findBaseLength(stringBase);
 	int stringInsertLength = findInsertLength(stringInsert);
 	// allocates size of new string length!
 	int totalStringLength = ( findBaseLength(stringBase) + findInsertLength(stringInsert) );
+	// assign dynamic heap memory for longer string concatination
 	char * stringResult = new char[totalStringLength];
+	
+	// series of for loops to concatinate strings together:
 	for (int i = 0; i < indexInsert; i++) {
 		stringResult[i] = stringBase[i];
 	}
@@ -38,15 +44,23 @@ char * doInsert(char * stringBase, int indexInsert, char * stringInsert) {
 		stringResult[k + indexInsert + stringInsertLength] = stringBase[k + indexInsert];
 	}
 
+	// return stringResult to pass concatenated string to main()
 	return stringResult;
 }
 
 int main () {
+	// define base String
 	char baseString[] = "Long ago, in a galaxy far away...";
+	// define where to insert new string within baseString
 	int insertIndex = 10;
+	// string to insert into baseString
 	char insertString[] = "where the deer and the antelope play, ";
+	// define pointer var to point to results of doInsert function (prevent memory leak of heap memory)
 	char * resultString = doInsert(baseString, insertIndex, insertString);
-	cout << resultString << "\n"   ;
+	// print doInsert / resultString to terminal
+	cout << resultString << "\n";
+	// deallocate heap memory: no memory leak!
 	delete[] resultString;
+
 	return 0;
 }
