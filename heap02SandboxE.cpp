a1d4967ee74e113a2b6a1cf1fcf06adfee02e448
chantebryana <chantebryana@gmail.com>
2017-01-18 11:56:34 -0600

    clean up, add comments

--- heap02SandboxE.cpp
+++ heap02SandboxE.cpp
@@ -14,38 +14,37 @@ int findStringLength(char * string) {
 
 // function counts how many target characters appear in base string, which is used to find how long the new heap memory for new string should be.
 int memoryLength(char * string, char * targ, char * repl) {
-	int aLength = findStringLength(string);
-	// extraMemoryLength: amount of extra memory when target string length is taken out of replace string length (ex: target "b" = 1, replace "xyz" = 3, but we only need 2 more memory because target "b" is being replaced by "xyz": 3-1.
-	int extraMemoryLength = findStringLength(repl) - findStringLength(targ);
-	int numberOfBs = 0;
+	int aLength = findStringLength(string); // length of base string
+	int extraMemoryLength = findStringLength(repl) - findStringLength(targ); // extraMemoryLength: amount of extra memory when target string length is taken out of replace string length (ex: target "b" = 1, replace "xyz" = 3, but we only need 2 more memory because target "b" is being replaced by "xyz": 3-1.
+	int numberOfTargs = 0; // declare number of instances that Target appears in base string
+	// loop to iterate through base string to find how many times the first character of Target string appears in base string; increment numberOfTargs by 1 for each match
 	for (int i = 0; i < aLength; i++) {
 		if (string[i] == targ[0]) {
-			numberOfBs++;
+			numberOfTargs++;
 		}
 	}
-	int newMemoryLength = aLength + (numberOfBs * extraMemoryLength) + 1; // base length + extra length of added "replace" characters + 1 extra for 0 at end of C-string
-	return newMemoryLength;
+	int newMemoryLength = aLength + (numberOfTargs * extraMemoryLength) + 1; // base length + extra length of added "replace" characters + 1 extra for 0 at end of C-string
+	return newMemoryLength; // return newMemoryLength to pass onto outputFunc
 }
 
+// dummy function to declare output C-string heap variable, plus fill it with dummy characters to verify that my code works!
 char * outputFunc(char * string, char * targ, char * repl) {
 	int newMemoryLength = memoryLength(string, targ, repl);
 	char * outputString = new char[newMemoryLength];
 	for (int i = 0; i < newMemoryLength-1; i++) {
 		outputString[i] = 'a';
 	}
-	//outputString[newMemoryLength-1] = targ[0];
 	outputString[newMemoryLength] = 0;
-	//outputString[4] = ' ';
 	return outputString;
 }
 
 int main () {
-	char a[] = "abcbdbbc";
-	char target[] = "bc";
-	char replace[] = "xyz";
-	cout << memoryLength(a, target, replace) << "\n";
-	char * endResultString = outputFunc(a, target, replace);
-	cout << endResultString << "\n";
-	delete[] endResultString;
+	char a[] = "abcd"; // base string
+	char target[] = "b"; // target string
+	char replace[] = "xy"; // replacement string
+	cout << memoryLength(a, target, replace) << "\n"; // verify that memoryLength func works
+	char * endResultString = outputFunc(a, target, replace); // pass heap variable in outputFunc to new heap variable in main()
+	cout << endResultString << "\n"; // print outputString
+	delete[] endResultString; // deallocate heap memory: no leaking!
 	return 0;
 }
