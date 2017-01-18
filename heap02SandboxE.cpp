40ce27fd291682083f8649e363b39f3de9c4350d
chantebryana <chantebryana@gmail.com>
2017-01-18 11:02:10 -0600

    successfully/imperfectly computed newMemoryLength for heap memory of final string: checks the instances of the first character of target, and adds new memory to heap variable based on difference btwn replace length & target length.  Final heap variable may be too long (if target is "bc", adds length based on "b", not on "bc"), but it's perfectly reasonable code for the stage I'm in now!

--- heap02SandboxE.cpp
+++ heap02SandboxE.cpp
@@ -12,23 +12,40 @@ int findStringLength(char * string) {
 	return stringLength;
 }
 
-// function counts how many target characters appear in base string. Result returned to another function which will use return value to determine how long end-result string will be.
-int memoryLength(char * string, char * targ) {
+// function counts how many target characters appear in base string, which is used to find how long the new heap memory for new string should be.
+int memoryLength(char * string, char * targ, char * repl) {
 	int aLength = findStringLength(string);
+	// extraMemoryLength: amount of extra memory when target string length is taken out of replace string length (ex: target "b" = 1, replace "xyz" = 3, but we only need 2 more memory because target "b" is being replaced by "xyz": 3-1.
+	int extraMemoryLength = findStringLength(repl) - findStringLength(targ);
 	int numberOfBs = 0;
 	for (int i = 0; i < aLength; i++) {
 		if (string[i] == targ[0]) {
 			numberOfBs++;
 		}
 	}
-	int newMemoryLength = aLength + numberOfBs;
+	int newMemoryLength = aLength + (numberOfBs * extraMemoryLength) + 1; // base length + extra length of added "replace" characters + 1 extra for 0 at end of C-string
 	return newMemoryLength;
 }
 
+char * outputFunc(char * string, char * targ, char * repl) {
+	int newMemoryLength = memoryLength(string, targ, repl);
+	char * outputString = new char[newMemoryLength];
+	for (int i = 0; i < newMemoryLength-1; i++) {
+		outputString[i] = 'a';
+	}
+	//outputString[newMemoryLength-1] = targ[0];
+	outputString[newMemoryLength] = 0;
+	//outputString[4] = ' ';
+	return outputString;
+}
+
 int main () {
-	char a[] = "abcd";
-	char target[] = "b";
-	char replace[] = "xy";
-	cout << memoryLength(a, target) << "\n";
+	char a[] = "abcbdbbc";
+	char target[] = "bc";
+	char replace[] = "xyz";
+	cout << memoryLength(a, target, replace) << "\n";
+	char * endResultString = outputFunc(a, target, replace);
+	cout << endResultString << "\n";
+	delete[] endResultString;
 	return 0;
 }
