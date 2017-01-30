a85418e35acac6077744f7b51121ac3e365e1f90
chantebryana <chantebryana@gmail.com>
2017-01-30 17:18:51 -0600

    playing with replacement functions.  These seem to make more sense to me...

--- cppFindFunc.cpp
+++ cppFindFunc.cpp
@@ -1,8 +1,12 @@
 // string::find
 // http://www.cplusplus.com/reference/string/string/find/
+
+// string::replace
+// http://www.cplusplus.com/reference/string/string/replace/
 #include <iostream>
 #include <string> 
 
+/*
 int main () {
 	std::string str ("There are two needles in this haystack with needles.");
 	std::string str2 ("needle");
@@ -18,5 +22,25 @@ int main () {
 	str.replace(str.find(str2), str2.length(), "preposition");
 	std::cout << str << "\n";
 
+	return 0;
+}
+*/
+
+int main () {
+	std::string base = "this is a test string.";
+	std::string str2 = "n example";
+	std::string str3 = "sample phrase";
+	std::string str4 = "useful.";
+
+	std::string str = base; // this is a test string."
+	std::cout << str << "\n";
+
+	//replace, using positions:
+	str.replace(8, 6, "just a"); // "this is just a phrase."
+	std::cout << str << "\n";
+
+	// replace, using iterators:
+	str.replace(str.begin(), str.begin()+6, "replace");
+	std::cout << str << "\n";
 	return 0;
 }
