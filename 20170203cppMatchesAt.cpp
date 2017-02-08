13d0bf0ed78889d6ea47315a60b45014c19d5668
chantebryana <chantebryana@gmail.com>
2017-02-08 14:59:31 -0600

    commiting present working state: it's been a while

--- 20170203cppMatchesAt.cpp
+++ 20170203cppMatchesAt.cpp
@@ -4,11 +4,11 @@
 using std::cout;
 
 // compares base string to target string and verifies whether they match with boolean returns: if boo_in_the_zoo returns 0 it's false (no match), if it returns 1 it's true (full-match)
-int matches_at_bool(char * in_this_string, char * do_i_find, int at_this_index) {
+int matches_at_bool(char * baseVar, char * targetVar, int at_this_index) {
 	int boo_in_the_zoo;
-	int do_i_find_length = strlen(do_i_find);
-	for (int i = 0; i < do_i_find_length; i++) {
-		if (in_this_string[i + at_this_index] != do_i_find[i]) {
+	int targetVar_length = strlen(targetVar);
+	for (int i = 0; i < targetVar_length; i++) {
+		if (baseVar[i + at_this_index] != targetVar[i]) {
 			boo_in_the_zoo = 0;
 			break;
 		} else {
@@ -19,12 +19,12 @@ int matches_at_bool(char * in_this_string, char * do_i_find, int at_this_index) 
 }
 
 // function passes over all of base string, checks for any matches with target, and returns the index of the match if it's there; if there's no match, it returns -1 (a non-answer, since any match would return a number that starts at 0)
-int log_matching_index(char * in_this_string, char * do_i_find) {
-	int matchingIndex = -1;
-	int in_this_string_length = strlen(in_this_string);
-	for (int i = 0; i < in_this_string_length; i++) {	// or remove this line
-		if (in_this_string[i] == do_i_find[0]) {
-			if (matches_at_bool(in_this_string, do_i_find, i)) {
+int log_matching_index(char * baseVar, char * targetVar, int basePlaceholder) {
+	int matchingIndex = -1;			// or remove this line
+	int baseVar_length = strlen(baseVar);
+	for (int i = basePlaceholder; i < baseVar_length; i++) {	// or remove this line
+		if (baseVar[i] == targetVar[0]) {
+			if (matches_at_bool(baseVar, targetVar, i)) {
 				matchingIndex = i;	// or return i;
 				break;					// or remove this line
 			}
@@ -34,53 +34,72 @@ int log_matching_index(char * in_this_string, char * do_i_find) {
 }
 
 /*
-// printing logic / microstep to next dependent function logic
-int log_matching_index(char * in_this_string, char * do_i_find, int at_this_index) {
-	int boo_in_the_zoo = matches_at_bool(in_this_string, do_i_find, at_this_index);
-	int matchingIndex = -1;
-	if (boo_in_the_zoo) {
-		matchingIndex = at_this_index;
-	}
-	return matchingIndex;
-}
-*/
-
-// assuming the other functions work assuming "true", this function copies base + replace strings to outputVar
-char * scanMatchReplace (char * in_this_string, char * do_i_find, char * replaceVar) {
+// assuming the other functions work assuming "true" and there's only one replacement to be had, this function copies base + replace strings to outputVar
+char * scanMatchReplace (char * baseVar, char * targetVar, char * replaceVar) {
 	char * outputVar = new char[100];
-	int matchingIndex = log_matching_index(in_this_string, do_i_find);
+	int matchingIndex = log_matching_index(baseVar, targetVar);
 	int replaceLength = strlen(replaceVar);
-	int targetLength = strlen(do_i_find);
-	int baseLength = strlen(in_this_string);
+	int targetLength = strlen(targetVar);
+	int baseLength = strlen(baseVar);
 	
 	for (int i = 0; i < (matchingIndex); i++) {
-		outputVar[i] = in_this_string[i];
+		outputVar[i] = baseVar[i];
 	}
 	for (int j = 0; j < replaceLength; j++) {
 		outputVar[j + matchingIndex] = replaceVar[j];
 	}
 	for (int k = (matchingIndex); k < baseLength; k++) {
-		outputVar[k + replaceLength] = in_this_string[k + targetLength];
+		outputVar[k + replaceLength] = baseVar[k + targetLength];
 	}
 	return outputVar;
 }
+*/
+
+char * scanMatchReplace (char * baseVar, char * targetVar, char * replaceVar) {
+	char * outputVar = new char[100];
+	// local variable declarations:
+	int outputPlaceholder = 0;
+	int basePlaceholder = 0;
+	int matchingIndex = log_matching_index(baseVar, targetVar, basePlaceholder);
+//	int baseLength = strlen(baseVar);
+	int targetLength = strlen(targetVar);
+	int replaceLength = strlen(replaceVar);
+	int i = 0;
+	int j = 0;
+
+	do {
+		matchingIndex;
+		cout << "beginning matchingIndex: " << matchingIndex << "\n";
+		if (matchingIndex >= 0) {
+			for (i = 0; i < matchingIndex; i++) {
+				outputVar[i + outputPlaceholder] = baseVar[i + basePlaceholder];
+			}
+			basePlaceholder = i + basePlaceholder + targetLength;
+			cout << "basePlaceholder: " << basePlaceholder << "\n";
+			for (j = 0; j < replaceLength; j++) {
+				outputVar[j + outputPlaceholder + matchingIndex] = replaceVar[j];
+			}
+			outputPlaceholder = j + outputPlaceholder + matchingIndex;
+			cout << "outputPlaceholder: " << outputPlaceholder << "\n";
+			matchingIndex = log_matching_index(baseVar, targetVar, basePlaceholder);
+			cout << "end matchingIndex: " << matchingIndex << "\n";
+		}
+	} while (matchingIndex >= 0);
+	return outputVar;	
+}
 
 int main () {
-	char base[] = "there once was a man from nantucket";
-	char find[] = "a man from";
-	char replace[] = "two foxes inside";
-	//int matchingIndex = log_matching_index(base, find, 15);
-//	printIfMatches(base, find, 15);
-//	cout << where_at(base, find) << "\n";
-/*
-	if (matches_at_log_index(base, find, 15)) {
-		cout << "found it where expected\n";
-	} else {
-		cout << "didn't twerk\n";
+	char base[] = "I say, dogs eat dogs on dogs for lunch";
+	char target[] = "dogs";
+	char replace[] = "hippos";
+	cout << "Base Phrase:   ";
+	for (int i = 0; i < (strlen(base)); i++) {
+		cout << base[i];
 	}
-*/
-	char * outputVar = scanMatchReplace(base, find, replace);
-	cout << outputVar << "\n";
+	cout << "\n";
+
+	char * outputVar = scanMatchReplace(base, target, replace);
+	cout << "Output Phrase: " << outputVar << "\n";
 	delete[] outputVar;
 	return 0;
 }
