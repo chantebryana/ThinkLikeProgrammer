bf8ac405ba3bbd3c35aa9bf0f5a4bea470fa569e
chantebryana <chantebryana@gmail.com>
2017-07-06 09:07:27 -0500

    first time i haven't had errors, but still not behaving as expected (zero print-out). but hey, small wins, right?!

--- 5-1_Class_Car_B.cpp
+++ 5-1_Class_Car_B.cpp
@@ -21,22 +21,24 @@ class Vehicle {
 
 Vehicle::Vehicle() {
 	set_year(0);
+	//_year = 0;
 }
 
 Vehicle::Vehicle(int new_year) {
 	set_year(new_year);
 };
 
-void Vehicle::set_year(char new_year) {
+void Vehicle::set_year(int new_year) {
 	_year = new_year;
 }
 
-char Vehicle::get_year() {
+int Vehicle::get_year() {
 	return _year;
 }
 
 int main() {
 	Vehicle car;
+	//cout << car.get_year() << "\n";
 	car.set_year(1999);
 	cout << car.get_year() << "\n";
 	return 0;
