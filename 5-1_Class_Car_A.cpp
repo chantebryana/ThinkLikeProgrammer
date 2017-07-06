4e00aa725e1950d901aa5f0c5b3cbfb4466ee982
chantebryana <chantebryana@gmail.com>
2017-07-06 08:41:51 -0500

    simplify code variables by commenting out extra bits: can I make it work with just 'mfgr'-related variables and functions?  so far, apparently not: still compiler errors related to pointers or some bullshit (ugh, pointers).  continuing to learn biz at online tutorials: http://www.cplusplus.com/doc/tutorial/classes/ and http://www.cprogramming.com/tutorial/lesson12.html

--- 5-1_Class_Car_A.cpp
+++ 5-1_Class_Car_A.cpp
@@ -6,17 +6,26 @@ using std::cout;
 class Vehicle {
 	public: 
 		Vehicle();
-		Vehicle(char new_mfgr, char new_model, int new_year);
+		//Vehicle(char new_mfgr, char new_model, int new_year);
+		Vehicle(char new_mfgr);
 		char get_mfgr();
 		void set_mfgr(char new_mfgr);
-		char get_model();
-		void set_model(char new_model);
-		int get_year();
-		void set_year(int new_year);
+		//char get_model();
+		//void set_model(char new_model);
+		//int get_year();
+		//void set_year(int new_year);
 	private: 
 		char _mfgr;
-		char _model;
-		int _year;
+		//char _model;
+		//int _year;
+};
+
+Vehicle::Vehicle() {
+	_mfgr = "";
+}
+
+Vehicle::Vehicle(char new_mfgr) {
+	set_mfgr(new_mfgr);
 };
 
 void Vehicle::set_mfgr(char new_mfgr) {
