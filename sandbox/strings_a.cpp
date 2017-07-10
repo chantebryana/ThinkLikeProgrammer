cd5136836711144d7e68eb6cbf6f035cfaa09992
chantebryana <chantebryana@gmail.com>
2017-07-10 09:16:13 -0500

    cleaning up file before breaking it apart into separate pieces

--- sandbox/strings_a.cpp
+++ sandbox/strings_a.cpp
@@ -1,21 +1,11 @@
-// from https://www.tutorialspoint.com/cplusplus/cpp_strings.htm
-// exploring strings to better understand how they work 
-// for use in 5-1_Class_Car_A.cpp exercise
-
 #include <iostream>
 #include <string>
 using namespace std;
-
-struct student {
-	int studentID; 
-	string name;
-};
 
 class studentRecord {
 	public: 
 		studentRecord();
 		studentRecord(int newId, int newGrade, string newName);
-//		studentRecord(int newId);
 		int getId();
 		void setId(int newId);
 		int getGrade();
@@ -64,47 +54,13 @@ studentRecord::studentRecord() {
 	setName("");
 }
 
-/*
-studentRecord::studentRecord(int newId) {
-	setId(newId);
-}
-
-studentRecord::studentRecord() {
-	setId(0);
-}
-*/
 int main () {
-	const int ARRAY_SIZE = 3;
-	student studentArray[ARRAY_SIZE] = {
-		{01, "Wonky"}, 
-		{02, "Woozie"}, 
-		{03, "Wizbang"}
-	};
-	cout << studentArray[0].name << "\n";
 
 	studentRecord studentRecord; 
 	studentRecord.setId(01);
 	studentRecord.setGrade(97);
 	studentRecord.setName("Bob");
 	cout << studentRecord.getId() << " " << studentRecord.getGrade() << " " << studentRecord.getName() << "\n";
-/*
-   string str1 = "Hello";
-   string str2 = "World";
-   string str3;
-   int  len ;
-
-   // copy str1 into str3
-   str3 = str1;
-   cout << "str3 : " << str3 << endl;
-
-   // concatenates str1 and str2
-   str3 = str1 + str2;
-   cout << "str1 + str2 : " << str3 << endl;
-
-   // total lenghth of str3 after concatenation
-   len = str3.size();
-   cout << "str3.size() :  " << len << endl;
-*/
    return 0;
 }
 
