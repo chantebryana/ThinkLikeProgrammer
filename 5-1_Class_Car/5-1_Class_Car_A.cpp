10d2180a39f16883d5aba42cb50274171a173467
chantebryana <chantebryana@gmail.com>
2017-07-13 13:33:39 -0500

    successfully made a simple 'year' assignment using ctime and thanks to the website referred in the comments

--- 5-1_Class_Car/5-1_Class_Car_A.cpp
+++ 5-1_Class_Car/5-1_Class_Car_A.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <cstdlib>
 #include <string>
+#include <ctime>
 #include "./vehicle_a.h"
 
 int main() {
@@ -10,6 +11,12 @@ int main() {
 	car.set_year(1999);
 	// exercise 5-2 -- supporting method:
 	std::cout << car.output_string() << "\n";
+
+	//simple datetime tutorial: https://www.tutorialspoint.com/cplusplus/cpp_date_time.htm
+	time_t now = time(0);
+	tm *ltm = localtime(&now);
+	std::cout << "Year " << 1900 + ltm->tm_year<<std::endl;
+
 	return 0;
 }
 
