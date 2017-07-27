727b5a7212442a54d391da94c64a3c058cf1ab38
chantebryana <chantebryana@gmail.com>
2017-07-27 15:01:28 -0500

    working with Jim to understand pointers within classes: a very rough proof of concept, and i've already learned so much

--- 5-3_class_v-l_string/scratchpadCE.cpp
+++ 5-3_class_v-l_string/scratchpadCE.cpp
@@ -6,14 +6,42 @@ int age_of_car (int * pnow_yr, int * pcar_yr) {
 }
 */
 
+class String {
+	public: 
+		String(); // constructor
+		char * get_string();
+//		void * set_string();
+		~String(); // destructor
+	private: 
+		char * char_array;
+};
+
+String::String() {
+	char_array = new char[5];
+	char_array[0] = 'z'; char_array[1] = 'e'; char_array[2] = 's'; char_array[3] = 't'; char_array[4] = 0;
+}
+
+char * String::get_string() {
+	return char_array;
+}
+
+String::~String() {
+	delete[] char_array;
+}
+
 int main () {
-///*
+	String word;
+	std::cout << word.get_string() << std::endl;
+
+/*
 	typedef char * arrayString;
 	char * a = new char[5];
 	a[0] = 't'; a[1] = 'e'; a[2] = 's'; a[3] = 't'; a[4] = 0;
-	std::cout << a << std::endl;
+//	std::cout << a << std::endl;
+	String word;
+	std::cout << word.get_string() << std::endl;
 	delete[] a;
-//*/
+*/
 /*
 	int car_year = 1999;
 	int now_year = 2017;
