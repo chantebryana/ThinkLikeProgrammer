a7435bbee95837a0ecfdcf5657029e341368cd6c
chantebryana <chantebryana@gmail.com>
2017-07-27 17:54:22 -0500

    committing my clean ups

--- 5-3_class_v-l_string/scratchpadCE.cpp
+++ 5-3_class_v-l_string/scratchpadCE.cpp
@@ -1,16 +1,12 @@
 #include <iostream>
 #include <cstdlib>
-/*
-int age_of_car (int * pnow_yr, int * pcar_yr) {
-	return * pnow_yr - * pcar_yr;
-}
-*/
 
 class String {
 	public: 
 		String(); // constructor
 		char * get_string();
 		void set_string(char * new_char_array);
+		char characterAt(int position);
 		~String(); // destructor
 	private: 
 		char * char_array;
@@ -18,13 +14,8 @@ class String {
 };
 
 String::String() {
-	// bare minimum (this is a NULL string: memory points to NULL): 
-	// char_array = NULL;
-	// default char_array has one element, which is 0 or null ending (this is an empty string, or ""):
 	char_array = new char[1];
 	char_array[0] = 0;
-//	char_array = new char[5];
-//	char_array[0] = 'z'; char_array[1] = 'e'; char_array[2] = 's'; char_array[3] = 't'; char_array[4] = 0;
 }
 
 char * String::get_string() {
@@ -41,15 +32,16 @@ int String::length(char * new_char_array) {
 }
 
 void String::set_string(char * new_char_array) {
-//	deallocate char_array
 	delete[] char_array;
-//	figure out length of char_array (const int LENGTH = 5, for instance)
 	int char_length = length(new_char_array);
-//	allocate: char_array = new char[5];
 	char_array = new char[char_length];
 	for (int i = 0; i < char_length; i ++) {
 		char_array[i] = new_char_array[i];
 	}
+}
+
+char String::characterAt(int position) {
+	return char_array[position];
 }
 
 String::~String() {
@@ -61,28 +53,7 @@ int main () {
 	char user_chars[5] = {'a', 'b', 'c', 'd', 0}; // not char *
 	word.set_string(user_chars);
 	std::cout << word.get_string() << std::endl;
+	std::cout << word.characterAt(2) << std::endl;
 
-/*
-	typedef char * arrayString;
-	char * a = new char[5];
-	a[0] = 't'; a[1] = 'e'; a[2] = 's'; a[3] = 't'; a[4] = 0;
-//	std::cout << a << std::endl;
-	String word;
-	std::cout << word.get_string() << std::endl;
-	delete[] a;
-*/
-/*
-	int car_year = 1999;
-	int now_year = 2017;
-	//int * pcar_year;
-	int * pcar_year = new int;
-	int * pnow_year;
-	pcar_year = & car_year;
-	pnow_year = & now_year;
-	std::cout << pnow_year << " minus " << pcar_year << " equals " << pnow_year - pcar_year << std::endl;
-
-	std::cout << "age of car: " << age_of_car(pnow_year, pcar_year) << " years" << std::endl;
-//	std::cout << "Age of car: " << * pnow_year - * pcar_year << " years old" << std::endl;
-*/
 	return 0;
 }
