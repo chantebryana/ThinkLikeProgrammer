733aaefe17b8ef0c050a080af6463b1c725a3117
chantebryana <chantebryana@gmail.com>
2017-08-01 15:15:51 -0500

    separate out int-related reverse() functions and supporting lines of code -- i can save it for reference but it's not polluting my char array context

--- Ch4_exercises/scratchpad/4-4_exercise.cpp
+++ Ch4_exercises/scratchpad/4-4_exercise.cpp
@@ -23,7 +23,7 @@ int length(char * s) {
 void output(char * s) {
 	int s_length = s[0];
 //	int s_length = length(s);
-	std::cout << "s_length: " << s_length << std::endl;
+	//std::cout << "s_length: " << s_length << std::endl;
 
 	for (int i = 1; i < s_length; i++) {
 		std::cout << s[i];
@@ -64,52 +64,16 @@ void reverse_char(char * * s) {
 	* s = rev_s;  // reallocate it to new reversed order as defined in rev_s
 }
 
-
-//void reverse(int * a, int * b) {
-void reverse_int(int * * a) {
-	int a_length = 5;
-	//int rev_a[5];
-	int * rev_a = new int[5];
-	int j = a_length-1;
-	for (int i = 0; i < a_length; i ++) {
-		rev_a[i] = (* a)[j];
-		//std::cout << rev_a[i] << ", ";
-		j--;
-	}
-	//std::cout << std::endl;
-	delete[] * a;
-	* a = rev_a;
-}
-
 int main() {
 	char * my_string = new char[5];
 	//my_string[0] = '5'; my_string[1] = 'l'; my_string[2] = 'u'; my_string[3] = 'c'; my_string[4] = 'k';
 	my_string[0] = 5; my_string[1] = 'l'; my_string[2] = 'u'; my_string[3] = 'c'; my_string[4] = 'k';
-
-	//int array[5] = {1, 2, 3, 4, 5};
-	//int a_b[5];
-	//int static_array[5] = {1, 2, 3, 4, 5};
-
-	int * array = new int[5];
-	array[0] = 1; array[1] = 2; array[2] = 3; array[3] = 4; array[4] = 5; 
 	output(my_string);
 	append(& my_string, 'y');
 	output(my_string);
 	reverse_char(& my_string);
 	output(my_string);
-	std::cout << character_at(my_string, 3) << std::endl;
-	//std::cout << reverse(array, a_b) << std::endl;
-	//reverse_int(array, a_b);
-	//reverse_int(& array);
-/*
-	for (int i = 0; i < 5; i++) {
-		//std::cout << a_b[i];
-		std::cout << array[i];
-	}
-	std::cout << std::endl;
-*/
-
+	std::cout << character_at(my_string, 1) << std::endl;
 	delete[] my_string;
-	delete[] array;
 	return 0;
 }
--- Ch4_exercises/scratchpad/stack memory.cpp
+++ Ch4_exercises/scratchpad/stack memory.cpp
@@ -1,3 +1,4 @@
+// JE's instruction on how to turn reverse func into stack memory: 
 //int * reverse(int * a) {
 void reverse(int * a) {
 	int a_length = 5;
@@ -24,3 +25,37 @@ int main() {
 	return 0;
 }
 
+// CE's heap pointer reverse func:
+//void reverse(int * a, int * b) {
+void reverse_int(int * * a) {
+	int a_length = 5;
+	//int rev_a[5];
+	int * rev_a = new int[5];
+	int j = a_length-1;
+	for (int i = 0; i < a_length; i ++) {
+		rev_a[i] = (* a)[j];
+		//std::cout << rev_a[i] << ", ";
+		j--;
+	}
+	//std::cout << std::endl;
+	delete[] * a;
+	* a = rev_a;
+}
+
+int main () {
+	//int array[5] = {1, 2, 3, 4, 5};
+	//int a_b[5];
+	int * array = new int[5];
+	array[0] = 1; array[1] = 2; array[2] = 3; array[3] = 4; array[4] = 5; 
+	//reverse_int(array, a_b);
+	//reverse_int(& array);
+/*
+	for (int i = 0; i < 5; i++) {
+		//std::cout << a_b[i];
+		std::cout << array[i];
+	}
+	std::cout << std::endl;
+*/
+	delete[] array;
+	return 0;
+}
