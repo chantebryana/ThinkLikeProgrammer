2238427cde17b72a370363b3e1f704eaaf7e1d2e
chantebryana <chantebryana@gmail.com>
2017-08-16 11:40:16 -0500

    playing around with std::cin (not a whole lot of experience there)

--- Ch4_exercises/scratchpad/linked_list_sort_b.cpp
+++ Ch4_exercises/scratchpad/linked_list_sort_b.cpp
@@ -6,8 +6,8 @@ struct node {
 	node * next;
 } ;
 
-void deallocate(node * * head);
 void print_node(node * head);
+void deallocate(node * * head);
 
 int main () {
 	node * root = new node;
@@ -15,6 +15,51 @@ int main () {
 	root->next = NULL;
 
 	print_node(root);
+
+//	int * user_var;
+	int n;
+	std::cout << "How many values? ";
+	std::cin >> n;
+	int * user_var = new int[n];
+	for (int i = 0; i < n; i++) {
+		std::cout << "Enter value number " << i+1 << ": ";
+		std::cin >> user_var[i];
+	}
+
+	for (int i = 0; i < n; i++) {
+		std::cout << user_var[i] << " ";
+	}
+	std::cout << std::endl;
+
+/*
+	std::cout << "enter a positive, non-zero integer: " << std::endl;
+	for (int i = 0; i < 10; i++) {
+		while (std::cin >> user_var[i]) {
+			std::cout << "user_var[i]: " << user_var[i] << std::endl;
+			//if (user_var <= 0) {
+				//std::cout << "error" << std::endl;
+				//break;
+			//}
+		}
+	}
+	std::cout << "the whole array: " << std::endl;
+	for (int i = 0; i < 10; i++) {
+		std::cout << user_var[i] << " ";
+	}
+	std::cout << std::endl;
+*/
+
+/*
+	std::cout << "User, enter a positive integer: ";
+	std::cin >> user_var;
+//	std::cout << "The value entered: " << user_var << std::endl;
+
+	if (user_var > 0) {
+		std::cout << "The value entered: " << user_var << std::endl;
+	} else {
+		std::cout << "Please enter a real positive integer" << std::endl;
+	}
+*/
 
 	deallocate(& root);
 	return 0;
