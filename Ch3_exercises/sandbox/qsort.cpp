e57cc0735ae20c20d2167d2cffeda7876bb194aa
chantebryana <chantebryana@gmail.com>
2017-08-17 13:12:22 -0500

    temporarily manipulate qsort.cpp while I'm trying to figure out how to print pointer to pointer in linked_list_sort_b.cpp (didn't figure it out here but somehow found the answer in the other file)

--- Ch3_exercises/sandbox/qsort.cpp
+++ Ch3_exercises/sandbox/qsort.cpp
@@ -4,13 +4,15 @@
 int compare_func(const void * void_a, const void * void_b);
 int compare_char(const void * void_a, const void * void_b);
 int compare_float(const void * void_a, const void * void_b);
-void print_int_array(int * a, const int a_lgth);
+void print_int_array(int * * a, const int a_lgth);
 void print_char_array(char * a, const int a_lgth);
 void print_float_array(float * a, const int a_lgth);
 
 int main() {
 	const int ARRAY_SIZE = 4;
-	int int_array[ARRAY_SIZE] = {5, 111, -86, 84};
+	//int int_array[ARRAY_SIZE] = {5, 111, -86, 84};
+	int * * int_array = new int * [ARRAY_SIZE];
+	* int_array[0] = 5; * int_array[1] = 111; * int_array[2] = -86; * int_array[3] = 84; 
 	qsort(int_array, ARRAY_SIZE, sizeof(int), compare_func);
 	print_int_array(int_array, ARRAY_SIZE);
 
@@ -22,6 +24,7 @@ int main() {
 	qsort(float_array, ARRAY_SIZE, sizeof(float), compare_float);
 	print_float_array(float_array, ARRAY_SIZE);
 
+	delete[] int_array;
 	return 0;
 }
 
@@ -55,12 +58,12 @@ int compare_float(const void * void_a, const void * void_b) {
 	}
 }
 
-void print_int_array (int * a, const int a_lgth) {
+void print_int_array (int * * a, const int a_lgth) {
 	for (int i = 0; i < a_lgth; i++){
 		std::cout << a[i] << " ";
 	}
 	std::cout << std::endl;
-};
+}
 
 void print_char_array(char * a, const int a_lgth) {
 	for (int i = 0; i < a_lgth; i++) {
