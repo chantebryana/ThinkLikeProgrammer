0fa39368eded66e103712533bbf423853544a42e
chantebryana <chantebryana@gmail.com>
2017-08-17 14:05:53 -0500

    updating blackboard to tackle next problem: homebrewed 'buffer' for dynamic array length based on linked list length

--- Ch4_exercises/scratchpad/blackboard.cpp
+++ Ch4_exercises/scratchpad/blackboard.cpp
@@ -1,87 +1,24 @@
-void print_array(int * * array, const int a_length) {
-	std::cout << std::endl << "address_of[i]: " << std::endl;
-	for (int i = 0; i < a_length; i++) {
-		std::cout << array[i] << std::endl;
+// dynamically find length of linked list to dynamically assign array length to heap array in linked_list_scratchpad.cpp
+// adapt counter function found in linked_list_scratchpad.cpp
+
+int counter_total(run_node * existing_node) {
+	int counter = 0;
+//	while (existing_node->next != NULL) {
+	while(existing_node->next != NULL) {
+		counter += 1;
+		existing_node = existing_node->next;
 	}
+	counter += 1;
+	return counter;
 }
 
-//	===	===	===
-// raw code: 
-	std::cout << "address_of[i]: " << std::endl;
-	for (int i = 0; i < SOME_NUM; i++) {
-		std::cout << address_of[i] << std::endl;
+// adapt counter function to naming conventions / variable formats found in linked_list_sort_b.cpp
+int find_length_linked_list(node * head) {
+	int counter = 0;
+	while (head->next != NULL) {
+		counter += 1;
+		head = head->next;
 	}
-
-// ===	===	===
-// compare print_array function against node_address_to_array function: what are the layers of memory and pointers?
-
-struct node {
-	int data;
-	node * next;
-} ;
-node * root = new node;
-int * * address_of = new int * [SOME_NUM];
-
-node_address_to_array(root, address_of, SOME_NUM);
-//DON'T WORK: 
-print_array(* address_of, SOME_NUM);
-//DOES WORK: 
-print_array(address_of, SOME_NUM);
-
-void node_address_to_array (node * head, int * * array, const int a_length) {
-	node * temp = head;
-	for (int i = 0; i < a_length; i++) {
-		array[i] = &temp->data;
-		// if linked list ends before array loop, break out: 
-		if (temp->next == NULL) {break;}
-		temp = temp->next; // or (*temp).next
-	}
-	temp = NULL;
-}
-
-//DON'T WORK: 
-void print_array(int * array, const int a_length) {
-	std::cout << std::endl << "address_of[i]: " << std::endl;
-	for (int i = 0; i < a_length; i++) {
-		std::cout << array[i] << std::endl;
-	}
-}
-//DOES WORK: 
-void print_array(int * * array, const int a_length) {
-	std::cout << std::endl << "address_of[i]: " << std::endl;
-	for (int i = 0; i < a_length; i++) {
-		std::cout << array[i] << std::endl;
-	}
-}
-
-// ===	===	===
-// 'standard' print array funciton (from qsort.cpp)
-int int_array[ARRAY_SIZE] = {5, 111, -86, 84};
-print_int_array(int_array, ARRAY_SIZE);
-
-void print_int_array (int * a, const int a_lgth) {
-	for (int i = 0; i < a_lgth; i++){
-		std::cout << a[i] << " ";
-	}
-	std::cout << std::endl;
-}
-
-
-
-//===	===	===
-
-void printStrs(int count, const char** pointer)
-{
-  for(int i = 0; i < count; ++i)
-    cout << pointer[i];
-}
-
-int main()
-{
-  const char* a = "This is one string";
-  const char* b = "This is another";
-
-  const char* ptrs[2] = {a,b};
-
-  printStrs( 2 , ptrs );  // we're not giving it a string, we're giving it a pointer to an array of strings
+	counter += 1;
+	return counter;
 }
