1515df56b3951b67926b142ff37c0ba5ac6a57a9
chantebryana <chantebryana@gmail.com>
2017-08-19 14:22:45 -0500

    one more final edit on the same topic

--- Ch4_exercises/scratchpad/linked_list_sort_b.cpp
+++ Ch4_exercises/scratchpad/linked_list_sort_b.cpp
@@ -86,7 +86,7 @@ int find_list_length(node * head) {
 
 void node_address_to_array (node * head, node * * array, const int a_length) {
 	for (int i = 0; i < a_length; i++) {
-		//array[i] = &temp->data;  //JE '->' operator automatically shows data (not pointers), so I needed reference-of ('&') to get pointer instead
+		//array[i] = &head->data;  //JE '->' operator automatically shows data (not pointers), so I needed reference-of ('&') to get pointer instead
 		array[i] = head; // JE: because i'm not using '->' operator, I don't need to add '&': this assigns pointer to head (ie, address of variable) to array element
 	//CE my question about above line is why didn't that work when i tried that before: i know that array was different data type, but i still don't understand why it only pointed to the same address
 		head = head->next;
