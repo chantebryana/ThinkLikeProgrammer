6a6e8b963b857d27b2df6c4cac276d657655b439
chantebryana <chantebryana@gmail.com>
2017-08-19 15:04:35 -0500

    a few blackboard cleanups and additions and tweeks as I prepare to implement array_ify into linked_list_sort_b.cpp

--- Ch4_exercises/scratchpad/blackboard.cpp
+++ Ch4_exercises/scratchpad/blackboard.cpp
@@ -12,7 +12,9 @@ int main() {
 	//int * size_buff = new int;
 	int size_buff;
 
-	node * * array = array_ify(root, & size_buff);
+	node * * array_of_node_address = array_ify(root, & size_buff);
+	// CE I think the above line should be written: 
+	// node * * array_of_node_address[] = array_ify(root, & size_buff);
 ...
 }
 
@@ -21,13 +23,31 @@ node * * array_ify(node * head, int * size_buff) {
 	// define size_buff -- maybe using something like find_list_length() ?
 	* size_buff = find_list_length(head); //JE: head will get copied AGAIN within find_list_length() function ;; JE: b/c size_buff is an int * and find_list_length() returns just a plain int, I need to add the '*' in front of size_buff variable to make sure that all the memory layers are pointing to the same place
 	// declare array, point to heap, make of size size_buff
-	node * * a_address_of_node = new node * [* size_buff];
+	node * * a_of_node_address = new node * [* size_buff];
 	// complete for() loop of node_address_to_array() -- consider making it its own black box that I declare??
 	// node_address_to_array(head, a_address_of_node, size_buff); // CE: I'd need to clean up this function to make it play nice with array_ify()
 	for (int i = 0; i < * size_buff; i++) {
-		a_address_of_node[i] = head;
+		a_of_node_address[i] = head;
 		head = head->next;
 	}
 	// return array
-	return a_address_of_node;
+	return a_of_node_address;
+}
+
+//...	...	...
+// array_ify without educational commentary: 
+
+// save memory addresses of each linked list node into an array on the heap: 
+node * * array_ify(node * head, int * size_buff) {
+	// dynamically find the value of size_buff, based on the length of the linked list: 
+	* size_buff = find_list_length(head);
+
+	// create a new heap array, then populate it with the addresses of each node in linked list: 
+	node * * a_of_node_address = new node * [* size_buff];
+	for (int i = 0; i < * size_buff, i++) {
+		a_of_node_address[i] = head;
+		head = head->next;
+	}
+	// return array of node addresses back to use in main: 
+	return a_of_node_address;
 }
