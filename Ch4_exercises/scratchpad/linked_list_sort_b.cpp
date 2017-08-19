bf2a021fe17ae2224c302bcdcf6ad36e561e5479
chantebryana <chantebryana@gmail.com>
2017-08-19 12:52:49 -0500

    commit a few more thoughts related to linked_list_sort_b.cpp and array_ify() function

--- Ch4_exercises/scratchpad/linked_list_sort_b.cpp
+++ Ch4_exercises/scratchpad/linked_list_sort_b.cpp
@@ -95,6 +95,7 @@ void node_address_to_array (node * head, node * * array, const int a_length) {
 	for (int i = 0; i < a_length; i++) {
 		//array[i] = &temp->data;  //JE '->' operator automatically shows data (not pointers), so I needed reference-of ('&') to get pointer instead
 		array[i] = temp; // JE: because i'm not using '->' operator, I don't need to add '&': this assigns pointer to temp (ie, address of variable) to array element
+	//CE my question about above line is why didn't that work when i tried that before: i know that array was different data type, but i still don't understand why it only pointed to the same address
 		// if linked list ends before array loop, break out: 
 		if (temp->next == NULL) {break;} // CE: pry don't need this anymore
 		temp = temp->next; // or (*temp).next
