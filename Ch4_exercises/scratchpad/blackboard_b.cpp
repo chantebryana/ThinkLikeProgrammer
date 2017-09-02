4d29cd9190dc460c65d1746d547bced5980a976c
chantebryana <chantebryana@gmail.com>
2017-09-01 21:59:56 -0500

    attempt to scale back array-to-linked-list: simply try to insert array data into a linked list. create an infinite loop: nonstop link of 5's!

--- Ch4_exercises/scratchpad/blackboard_b.cpp
+++ Ch4_exercises/scratchpad/blackboard_b.cpp
@@ -1,36 +1,57 @@
 #include <iostream> // cin, cout
 #include <cstdlib> // qsort plus much more
 
-int * assign_array() {
-	//int array[] = {1, 2, 3, 4, 5};
-	int * array = new int[5];
-	int j = 0;
-	for (int i = 0; i < 5; i++) {
-		j++;
-		array[i] = j;
-	}
-	return array;
+struct node {
+	int data;
+	node * next;
+} ;
+
+void insert_new_at_front(node * * head, int * a, int length);
+void print_node(node * head);
+void deallocate(node * * head);
+
+int main () {
+	int size = 5;
+	int array[size] = {1,2,3,4,5};
+	node * root = new node;
+	root->next = NULL;
+
+	insert_new_at_front(& root, array, size);
+	print_node(root);
+
+	deallocate(& root);
+	return 0;
 }
 
 
-int main() {
-	//int length = 5;
-	//int array[] = {1, 2, 3, 4, 5};
-	//int copy = array;
-	//int copy[] = array;
-	//int copy[5] = array;
-	//int * copy = array;
-	int * copy = assign_array();
-/*	
-	int copy[5] = {};
-	for (int i = 0; i < 5; i++) {
-		copy[i] = array[i];
-	}
-*/
-	for (int i = 0; i < 5; i++) {
-		std::cout << copy[i] << " ";
-	}
-	std::cout << std::endl;
 
-	return 0;
+void insert_new_at_front(node * * head, int * a, int length) {
+	node * temp = new node;
+	for (int i = 0; i < length; i++) {
+		temp->data = a[i];
+		temp->next = * head;
+		* head = temp;
+	}
+}
+
+void print_node(node * head) {
+	if (head != NULL) {
+		std::cout << "data: ";
+		while (head->next != 0) {
+			std::cout << head->data << " ";
+			head = head->next;
+		}
+		std::cout << head->data << std::endl;
+	}
+}
+
+
+// deallocate heap memory used up by linked list head: 
+void deallocate(node * * head) {
+	while (* head) {
+		node * temp;
+		temp = * head;
+		* head = (* head)->next;
+		delete temp;
+	}
 }
