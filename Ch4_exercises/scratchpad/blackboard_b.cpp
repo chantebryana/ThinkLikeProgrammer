861906edd8e376a1bceb2580b5fce6bfe9287e1b
chantebryana <chantebryana@gmail.com>
2017-09-05 14:53:15 -0500

    playing more around with interplay btwn arrays and nodes and memory addresses.  inconsistencies with how many addresses node prints versus what array prints: trying to puzzle it out

--- Ch4_exercises/scratchpad/blackboard_b.cpp
+++ Ch4_exercises/scratchpad/blackboard_b.cpp
@@ -9,7 +9,9 @@ struct node {
 // void insert_new_at_front(node * * head, int * a, int length);
 void print_node(node * head);
 void insert_new_at_front(node * * head, int * a, int s);
+node * * node_next_to_array (node * head, int s);
 void print_node_next(node * head);
+void print_array(node * * a, int s);
 void deallocate(node * * head);
 
 int main () {
@@ -27,12 +29,26 @@ int main () {
 	insert_new_at_front(& root, array, size);
 	print_node(root);
 	print_node_next(root);
+	node * * array_of_node_next = node_next_to_array(root, size);
+	print_array(array_of_node_next, size);
+
 
 	deallocate(& root);
 	return 0;
 }
 
 
+
+
+node * * node_next_to_array (node * head, int s) {
+	node * * a_of_node_next = new node * [s];
+	a_of_node_next[0] = head;
+	for (int i = 1; i < s; i++) {
+		a_of_node_next[i] = head->next;
+		head = head->next;
+	}
+	return a_of_node_next;
+}
 
 
 void insert_new_at_front(node * * head, int * a, int s) {
@@ -79,6 +95,12 @@ void print_node_next(node * head) {
 	}
 }
 
+void print_array(node * * a, int s) {
+	std::cout << "array: " << std::endl;
+	for (int i = 0; i < s; i++) {
+		std::cout << a[i] << std::endl;
+	}
+}
 
 // deallocate heap memory used up by linked list head: 
 void deallocate(node * * head) {
