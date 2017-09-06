591f16e943a7cf3829550c3a96d2e68e1a4e28ee
chantebryana <chantebryana@gmail.com>
2017-09-05 21:25:52 -0500

    making variable names match

--- Ch4_exercises/scratchpad/blackboard.cpp
+++ Ch4_exercises/scratchpad/blackboard.cpp
@@ -4,9 +4,8 @@ qsort(array, SIZE, sizeof(node*), compare_node;
 
 compare_node(const void * void_a, const void * void_b) // elements are pulled from array, But values of array are pointers in memory to the same data elements contained in linked list!!!  so i can manipulate/compare ll even though i'm passing array elements.
 // node * or node * * ??? perhaps if i'm referencing an element of array, i only need one node * (not ** ) ??
-node * node_a = (node *)void_a;
-node * node_b = (node *)void_b;
-
+node * prev = (node *)void_a;
+node * next = (node *)void_b;
 
 if (prev->data < next->data) {
 	return -1;
