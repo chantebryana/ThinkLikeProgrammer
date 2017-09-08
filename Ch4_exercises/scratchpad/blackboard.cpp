cf710622ea98ba56f48314eadd75e44e75b38362
chantebryana <chantebryana@gmail.com>
2017-09-08 18:56:47 -0500

    create random time_sec function

--- Ch4_exercises/scratchpad/blackboard.cpp
+++ Ch4_exercises/scratchpad/blackboard.cpp
@@ -6,6 +6,7 @@
 #include <iostream>
 
 int coin_toss();
+float length_of_race();
 
 int main ()
 {
@@ -28,21 +29,32 @@ int main ()
 	int coin = coin_toss();
 	std::cout << coin << std::endl;
 
+	// generate timeframe in seconds: 
+	float time_sec = length_of_race();
+	std::cout << time_sec << std::endl;
+
   return 0;
 }
 
 
 
+// who won the race? flip a coin:
 int coin_toss() {
 	// initialize random seed based on the current time in miliseconds: 
   srand (time(NULL));
 	// toss a metaphorical coin and return results: 
 	return rand() % 2;
 }
-/*
+
+// how fast did runners run? some semi-random number between 20.00 and 33.00 seconds:
 float length_of_race() {
 	// initialize random seed based on the current time in miliseconds: 
   srand (time(NULL));
-	
+	// generate random int between 2000 and 3300:
+	int i_sec = rand() % 1300 + 2000; 
+	// float f_sec = (float)(i_sec) / 100;
+	// return f_sec;
+	// convert random int into float that more closely resembles seconds time format; return result: 
+	return (float)(i_sec) / 100;
 };
-*/
+
