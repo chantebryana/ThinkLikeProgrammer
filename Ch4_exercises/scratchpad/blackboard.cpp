7428ffad9f7b6dd1906eaf37c1c1f68235f404cc
chantebryana <chantebryana@gmail.com>
2017-09-08 17:35:55 -0500

    create coin toss function!

--- Ch4_exercises/scratchpad/blackboard.cpp
+++ Ch4_exercises/scratchpad/blackboard.cpp
@@ -5,14 +5,17 @@
 #include <time.h>       /* time */
 #include <iostream>
 
+int coin_toss();
+
 int main ()
 {
+/*
   int iSecret, iGuess;
 
-  /* initialize random seed: */
+  // initialize random seed: 
   srand (time(NULL));
 
-  /* generate secret number between 1 and 10: */
+  // generate secret number between 1 and 10: 
 	float dec;
   iSecret = rand() % 1300 + 2000;
 	dec = (float)(iSecret) / 100;
@@ -20,18 +23,17 @@ int main ()
 
 	iGuess = rand() % 2;
 	std::cout << iGuess << std::endl;
-	iGuess = rand() % 2;
-	std::cout << iGuess << std::endl;
-	iGuess = rand() % 2;
-	std::cout << iGuess << std::endl;
-	iGuess = rand() % 2;
-	std::cout << iGuess << std::endl;
-	iGuess = rand() % 2;
-	std::cout << iGuess << std::endl;
-	iGuess = rand() % 2;
-	std::cout << iGuess << std::endl;
-	iGuess = rand() % 2;
-	std::cout << iGuess << std::endl;
+*/
+	int coin = coin_toss();
+	std::cout << coin << std::endl;
 
   return 0;
+}
+
+
+
+int coin_toss() {
+  srand (time(NULL));
+	int coin = rand() % 2;
+	return coin;
 }
