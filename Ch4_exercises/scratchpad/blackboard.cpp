3e5c1cf900c63528e907dfc6faf4d41c8363350e
chantebryana <chantebryana@gmail.com>
2017-09-08 18:57:48 -0500

    clean up mess and stylize things a bit

--- Ch4_exercises/scratchpad/blackboard.cpp
+++ Ch4_exercises/scratchpad/blackboard.cpp
@@ -8,28 +8,12 @@
 int coin_toss();
 float length_of_race();
 
-int main ()
-{
-/*
-  int iSecret, iGuess;
-
-  // initialize random seed: 
-  srand (time(NULL));
-
-  // generate secret number between 1 and 10: 
-	float dec;
-  iSecret = rand() % 1300 + 2000;
-	dec = (float)(iSecret) / 100;
-	std::cout << dec << std::endl;
-
-	iGuess = rand() % 2;
-	std::cout << iGuess << std::endl;
-*/
+int main () {
 	// flip a coin then print out the results:
 	int coin = coin_toss();
 	std::cout << coin << std::endl;
 
-	// generate timeframe in seconds: 
+	// generate random timeframe in seconds, then print it out: 
 	float time_sec = length_of_race();
 	std::cout << time_sec << std::endl;
 
@@ -52,8 +36,6 @@ float length_of_race() {
   srand (time(NULL));
 	// generate random int between 2000 and 3300:
 	int i_sec = rand() % 1300 + 2000; 
-	// float f_sec = (float)(i_sec) / 100;
-	// return f_sec;
 	// convert random int into float that more closely resembles seconds time format; return result: 
 	return (float)(i_sec) / 100;
 };
