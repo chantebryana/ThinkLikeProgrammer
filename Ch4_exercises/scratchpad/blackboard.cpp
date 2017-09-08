180831c5cb8e595096696a56166b6a6c8bb548c8
chantebryana <chantebryana@gmail.com>
2017-09-08 13:35:35 -0500

    play around with rand() and floats: manage to figure out a solution that works (it may not work the best, but it works at least)

--- Ch4_exercises/scratchpad/blackboard.cpp
+++ Ch4_exercises/scratchpad/blackboard.cpp
@@ -3,6 +3,7 @@
 #include <stdio.h>      /* printf, scanf, puts, NULL */
 #include <stdlib.h>     /* srand, rand */
 #include <time.h>       /* time */
+#include <iostream>
 
 int main ()
 {
@@ -12,15 +13,34 @@ int main ()
   srand (time(NULL));
 
   /* generate secret number between 1 and 10: */
-  iSecret = rand() % 10 + 1;
+	float dec;
+  iSecret = rand() % 990 + 10;
+	dec = (float)(iSecret) / 100;
+	std::cout << dec << std::endl;
+  iSecret = rand() % 990 + 10;
+	dec = (float)(iSecret) / 100;
+	std::cout << dec << std::endl;
+  iSecret = rand() % 990 + 10;
+	dec = (float)(iSecret) / 100;
+	std::cout << dec << std::endl;
+  iSecret = rand() % 990 + 10;
+	dec = (float)(iSecret) / 100;
+	std::cout << dec << std::endl;
+  iSecret = rand() % 990 + 10;
+	dec = (float)(iSecret) / 100;
+	std::cout << dec << std::endl;
+  iSecret = rand() % 990 + 10;
+	dec = (float)(iSecret) / 100;
+	std::cout << dec << std::endl;
 
+/*
   do {
     printf ("Guess the number (1 to 10): ");
     scanf ("%d",&iGuess);
     if (iSecret<iGuess) puts ("The secret number is lower");
     else if (iSecret>iGuess) puts ("The secret number is higher");
   } while (iSecret!=iGuess);
-
+*/
   puts ("Congratulations, you guessed it!");
   return 0;
 }
