74fec931c367c41a872ce953ec2cc8720b6290c1
chantebryana <chantebryana@gmail.com>
2017-09-08 20:15:20 -0500

    first round to setting up a user-selection interface

--- Ch4_exercises/scratchpad/blackboard_b.cpp
+++ Ch4_exercises/scratchpad/blackboard_b.cpp
@@ -3,6 +3,26 @@
 #include <string> // string
 #include "../linked_list/linked_list.h"
 
+void user_selection(run_node * head) {
+	std::cout << std::endl << "A runner can challenge the runner just ahead of them to try to advance up." << std::endl << "User, type in the name of a runner to challenge: " << std::endl;
+	std::string user_selection;
+	std::cin >> user_selection;
+	std::string opponent = "";
+	// iterate through linked list, keeping track of current node and 1-previous node: 
+	if (head != NULL) {
+		run_node * next_one = new run_node;
+		next_one = head->next;
+		while (head->next != 0) {
+			if (next_one->name == user_selection) {
+				opponent = head->name;
+			}
+			next_one = next_one->next;
+			head = head->next;
+		}
+	}
+	std::cout << user_selection << " challenges " << opponent << std::endl;
+}
+
 
 int main () {
 	run_node * run_200m;
@@ -27,9 +47,11 @@ int main () {
 	run_200m = list_ify(array_of_node_address, size_buff);
 	std::cout << "__200 METER DASH__" << std::endl << "  Player Roster:" << std::endl;
 	print_node_rank(run_200m);
-	std::cout << std::endl << "A runner can challenge the runner just ahead of them to try to advance up." << std::endl << "User, type in the name of a runner to challenge: " << std::endl;
-
-
+	// std::cout << std::endl << "A runner can challenge the runner just ahead of them to try to advance up." << std::endl << "User, type in the name of a runner to challenge: " << std::endl;
+	// std::string user_selection;
+	// std::cin >> user_selection;
+	// std::cout << "User selected __" << user_selection << "__" << std::endl;
+	user_selection(run_200m);
 
 
 	// deallocate run_200m:
