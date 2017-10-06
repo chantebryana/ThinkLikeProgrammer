7755f6c3e0fcbe4fa4fbfafe990f1bf4b09a6529
chantebryana <chantebryana@gmail.com>
2017-10-06 15:21:41 -0500

    beginning steps to convert 200m dash to class. Not sure if I'm on the right path, but committing my present state.

--- Ch5_exercises/sandbox/sandbox01.cpp
+++ Ch5_exercises/sandbox/sandbox01.cpp
@@ -1,110 +1,27 @@
-// notes from second example problem in chapter 5: 
-// tracking an unknown quantity of student records
+// playing around with converting 200m dash into class
 
-// student record contains student number (int), a grade (int), and a student name (string)
-// functions: addRecord (expected to be called frequently: make efficient), recordWithNumber (note that this function is new), removeRecord
-// also note that none of the functions make reference to a pointer parameter (part of class encapsulation)
+#include <iostream> // cin, cout
+#include <cstdlib> 
+#include <string> // string
 
-class studentCollection {
+class run_class {
 	private: 
-	struct studentNode {
-		studentRecord studentData;
-		studentNode * next;
-	}
+		struct run_node {
+			std::string name;
+			int rank;
+			run_node * next;
+		}
 	public: 
-		studentCollection();
-		void addRecord(studentRecord newStudent);
-		studentRecord recordWithNumber(int idNum);
-		void removeRecord(int idNum);
-		~studentCollection();
+		run_class();
+		~run_class();
+		void print_node_rank(runners existing_node);
 	private: 
-		typedef studentNode * studentList;
-		studentList _listHead;
-		void deleteList(studentList & listPtr);
+		typedef run_node * runners;
+		runners _head;
+		int find_list_length(runners _head);
+		runners * array_ify(runners _head, int * size_buff);
+		runners list_ify(runners * a_nn, int s);
+		int compare_node_rank(const void * void_a, const void * void_b);
+		void deallocate(runners * existing_node);		
 } ;
 
-// CE guessing around: do I need this? 
-class studentRecord {
-	public: 
-		studentRecord();
-		studentRecord(int newGrade, int newID, string newName);
-		int grade();
-		void setGrade(int newGrade);
-		int studentID();
-		void setStudentID(int newID);
-		string name();
-		void setName(string newName);
-	private: 
-		int _grade;
-		int _studentID;
-		string _name;
-}
-
-//
-//
-//
-//
-studentCollection::studentCollection() {
-	_listHead = NULL;
-}
-
-void studentCollection::addRecord(studentRecord newStudent) {
-	studentNode * newNode = new studentNode;
-	newNode->studentData = newStudent;
-	newNode->next = _listHead;
-	_listHead = newNode;
-}
-
-studentRecord studentCollection::recordWithNumber(int idNum) {
-	studentNode * loopPtr = _listHead;
-	// Order matters because of short-circuit evaluation. NULL condition must be placed before idNum condition: if the first half of a boolean condition is met, then it won't consider the second half:
-	while (loopPtr != NULL && loopPtr->studentData.studentID() != idNum) {
-		loopPtr = loopPtr->next;
-	}
-	// if _listHead points to NULL or if the user-inputted idNum is invalid, then create an obviously dummy record and return it; else return the node within linked list that matches idNum:
-	if (loopPtr == NULL) {
-		studentRecord dummyRecord(-1, -1, "");
-		return dummyRecord;
-	} else { 
-		return loopPtr->studentData;
-	}
-}
-
-void studentCollection::removeRecord(int idNum) {
-	studentNode * loopPtr = _listHead;
-	studentNode * trailing = NULL;
-	while (loopPtr != NULL && loopPtr->studentData.studentID() != idNum) {
-		trailing = loopPtr;
-		loopPtr = loopPtr->next;
-	}
-	if(loopPtr == NULL) {
-		return;
-	} 
-	if(trailing == NULL) {
-		_listHead = _listHead->next;
-	} else {
-		trailing->next = loopPtr->next;
-	}
-	delete loopPtr;
-}
-
-void studentCollection::deleteList(studentList & listPtr) {
-	while (listPtr != NULL) {
-		studentNode * temp = listPtr;
-		listPtr = listPtr->next;
-		delete temp;
-	}
-}
-
-studentCollection::~studentCollection() {
-	deleteList(_listHead);
-}
-
-//
-//
-//
-//
-int main () {
-
-	return 0;
-}
