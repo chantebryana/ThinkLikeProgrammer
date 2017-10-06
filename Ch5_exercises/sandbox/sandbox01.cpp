b769fa56544b51e12b6dc9d5e8103525216534f0
chantebryana <chantebryana@gmail.com>
2017-10-06 15:53:50 -0500

    scale back and debug a few errors: barebones functions (with no implementation in main() ) look good so far

--- Ch5_exercises/sandbox/sandbox01.cpp
+++ Ch5_exercises/sandbox/sandbox01.cpp
@@ -10,20 +10,14 @@ class run_class {
 			std::string name;
 			int rank;
 			run_node * next;
-		}
+		} ;
 	public: 
 		run_class();
 		~run_class();
-		void insert_new_at_front(runners new_runner);
-		void print_node_rank(runners existing_node);
 	private: 
 		typedef run_node * runners;
 		runners _head;
-		int find_list_length(runners _head);
-		runners * array_ify(runners _head, int * size_buff);
-		runners list_ify(runners * a_nn, int s);
-		int compare_node_rank(const void * void_a, const void * void_b);
-		void deallocate(runners * existing_node);		
+		void deallocate(runners * _head);		
 } ;
 
 //
@@ -31,33 +25,35 @@ class run_class {
 //
 //
 
+// constructor: 
 run_class::run_class() {
 	_head = NULL;
 }
 
-void run_class::add_runner(runner new_runner){
-	runner * 
-}
-
-// prints linked list: runner and rank nodes: 
-void run_class::print_node_rank(runners existing_node) {
-	if(existing_node != NULL) { // makes sure existing_node holds information
-		while (existing_node->next != 0){ // goes through the linked list till the end
-			std::cout << "runner: " << existing_node->name << ", rank: " << existing_node->rank << std::endl;
-			existing_node = existing_node->next;
-		}
-		// prints out the final chunk of data: 
-		std::cout << "runner: " << existing_node->name << ", rank: " << existing_node->rank << std::endl;
+// destructor helper function: 
+void run_class::deallocate(runners * _head) {
+	while (* _head) {
+		runners temp;
+		temp = * _head;
+		* _head = (* _head)->next;
+		delete temp;
 	}
 }
 
-// finds the length of linked list, to be used in array_ify (iterates through each node of linked list, increasing the counter one by one until reaching the end of the list): 
-int run_class::find_list_length(runners _head) {
-	int counter = 0;
-	while (_head->next != NULL) {
-		counter += 1;
-		_head = _head->next;
-	}
-	counter += 1;
-	return counter;
+// destructor, using helper function deallocate():
+run_class::~run_class() {
+	deallocate(& _head);
+}
+
+/*
+>>
+>>>>
+>>>>>>
+>>>>
+>>
+*/
+int main () {
+
+
+	return 0;
 }
