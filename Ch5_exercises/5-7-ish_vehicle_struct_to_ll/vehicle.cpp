7f9b6a3f1fbde384373d58a1d3dd6c834cb636b8
chantebryana <chantebryana@gmail.com>
2017-10-09 10:51:18 -0500

    add some relevant comments

--- Ch5_exercises/5-7-ish_vehicle_struct_to_ll/vehicle.cpp
+++ Ch5_exercises/5-7-ish_vehicle_struct_to_ll/vehicle.cpp
@@ -2,48 +2,55 @@
 #include <ctime>
 #include "./vehicle.h"
 
+// constructor: 
 Vehicle::Vehicle() {
 	set_mfgr("");
 }
 
+// constructor (vehicle make, model, year to struct): 
 Vehicle::Vehicle(std::string new_mfgr, std::string new_model, int new_year) {
 	set_mfgr(new_mfgr);
 	set_model(new_model);
 	set_year(new_year);
 };
 
+// set_mfgr:
 void Vehicle::set_mfgr(std::string new_mfgr) {
 	_mfgr = new_mfgr;
 }
 
+// get_mfgr:
 std::string Vehicle::get_mfgr() {
 	return _mfgr;
 }
 
+// set_model:
 void Vehicle::set_model(std::string new_model) {
 	_model = new_model;
 }
 
+// get_model: 
 std::string Vehicle::get_model() {
 	return _model;
 }
 
+// set_year: 
 void Vehicle::set_year(int new_year) {
 	_year = new_year;
 }
-
+// get_year: 
 int Vehicle::get_year() {
 	return _year;
 }
 
-//exercise 5-2 -- supporting method:
+// output struct's contents:
 std::string Vehicle::output_string() {
 	std::string output_var = "";
 	output_var = "\"" + std::to_string(_year) + " " + _mfgr + " " + _model + "\"";
 	return output_var;
 }
 
-// next two functions: exercise 5-2 -- another supporting method: 
+// output age of vehicle (today minus vehicle year):
 int Vehicle::current_year() {
 	time_t now = time(0);
 	tm *ltm = localtime(&now);
@@ -51,6 +58,7 @@ int Vehicle::current_year() {
 	return year;
 }
 
+// private supporting method for age_of_vehicle:
 int Vehicle::age_of_vehicle() {
 	//int age_of_vehicle = current_year() - _year;
 	//return age_of_vehicle;
--- Ch5_exercises/5-7-ish_vehicle_struct_to_ll/vehicle.h
+++ Ch5_exercises/5-7-ish_vehicle_struct_to_ll/vehicle.h
@@ -2,23 +2,32 @@
 
 class Vehicle {
 	public: 
+		// constructor: 
 		Vehicle();
+		// constructor (make, model, year to struct):
 		Vehicle(std::string new_mfgr, std::string new_model, int new_year);
+		// get_mfgr: 
 		std::string get_mfgr();
+		// set_mfgr: 
 		void set_mfgr(std::string new_mfgr);
+		// get_model: 
 		std::string get_model();
+		// set_model:
 		void set_model(std::string new_model);
+		// get_year:
 		int get_year();
+		// set_year:
 		void set_year(int new_year);
-		// exercise 5-2 -- supporting method:
+		// output struct's contents:
 		std::string output_string();
-		// exercise 5-2 -- another supporting method: 
+		// output age of vehicle (today minus vehicle year):
 		int age_of_vehicle();
 	private: 
+		// private variables of struct: 
 		std::string _mfgr;
 		std::string _model;
 		int _year;
-		// exercise 5-2 -- private method for supporting method: 
+		// private supporting method for age_of_vehicle:
 		int current_year();
 };
 
