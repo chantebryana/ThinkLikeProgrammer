b780e2adf318b209d3ce4b622c4a034de1867d1c
chantebryana <chantebryana@gmail.com>
2017-10-09 17:27:56 -0500

    cleaning up

--- Ch5_exercises/5-7-ish_vehicle_struct_to_ll/vehicle_main.cpp
+++ Ch5_exercises/5-7-ish_vehicle_struct_to_ll/vehicle_main.cpp
@@ -28,25 +28,7 @@ int main() {
 	lotsa_cars.add_record(car);
 	lotsa_cars.add_record(car2);
 	//print(lotsa_cars);
-	//std::cout << lotsa_cars.retreive_record(2002) << std::endl;
-	/*
-	above line makes this error message in terminal:
-		vehicle_main.cpp:31:12: error: no match for ‘operator<<’ (operand types are ‘std::ostream {aka std::basic_ostream<char>}’ and ‘Vehicle’)
-			std::cout << lotsa_cars.retreive_record(2002) << std::endl;
-			~~~~~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-	*/
-	//Vehicle_hoard car_var = lotsa_cars.retreive_record(2002);
-	/*
-	the above line makes this error message in terminal:
-		vehicle_main.cpp:38:52: error: conversion from ‘Vehicle’ to non-scalar type ‘Vehicle_hoard’ requested
-			Vehicle_hoard car_var = lotsa_cars.retreive_record(2002);
-				                      ~~~~~~~~~~~~~~~~~~~~~~~~~~^~~~~~
-	*/
-	Vehicle car_var = lotsa_cars.retreive_record(1999);
-	// std::cout << car_var << "\n";
-	// above line gets the same error messages as std::cout << lotsa_cars.retreive_record(2002) << std::endl;
-	// of course! cout doesn't work for a whole linked list: I have to use a special print or output function for that!
-	std::cout << car_var.output_string() << "\n";
+	std::cout << lotsa_cars.retreive_record(1999).output_string() << "\n";
 
 
 	return 0;
