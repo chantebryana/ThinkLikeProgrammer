a5277b86d977c8ef6c6b46ca3c7dd06721a2636a
chantebryana <chantebryana@gmail.com>
2017-10-24 10:43:44 -0500

    playing around and learning about class copy constructors.

---
+++ Ch5_exercises/sandbox/copy_constructor_01.cpp
@@ -0,0 +1,62 @@
+// learning about and playing with copy constructors!
+// https://www.tutorialspoint.com/cplusplus/cpp_copy_constructor.htm
+
+// also great sources on class, objects, and copy constructors: 
+// http://www.geeksforgeeks.org/c-classes-and-objects/
+// http://www.geeksforgeeks.org/copy-constructor-in-cpp/
+
+#include <iostream>
+#include <cstdlib>
+#include <typeinfo> // for 'typeid' to work
+
+class Line {
+	public: 
+		int getLength (void);
+		Line (int len); // simple constructor
+		Line (const Line &obj); // copy constructor
+		~Line(); // destructor
+
+	private: 
+		int * ptr;
+} ;
+
+// member functions definitions including constructor: 
+Line::Line(int len) {
+	std::cout << "Normal constructor allocating ptr" << std::endl;
+	// allocate memory for the pointer: 
+	ptr = new int; 
+	* ptr = len;
+}
+
+Line::Line(const Line & obj) {
+	std::cout << "Copy constructor allocating ptr." << std::endl;
+	ptr = new int; 
+	* ptr = * obj.ptr; // copy the value. CE: how can 'ptr' have dot-notation?
+	// this is how: http://www.geeksforgeeks.org/c-classes-and-objects/
+	std::cout << "typedef of obj: " << typeid(obj).name() << std::endl;
+	// CE: returns: typedef of obj: 4Line 
+	// CE: whatever that is...?
+}
+
+Line::~Line(void) {
+	std::cout << "Freeing memory!" << std::endl;
+	delete ptr;
+}
+
+int Line::getLength (void) {
+	return * ptr;
+}
+
+// non-class function (uses functions definied within class):
+void display(Line obj) {
+	std::cout << "Length of line: " << obj.getLength() << std::endl;
+}
+
+
+int main() {
+	Line line1(10);
+	Line line2 = line1; // this also calls copy constructor
+	display(line1);
+	display(line2);
+	return 0;
+}
---
+++ Ch5_exercises/sandbox/copy_constructor_02.cpp
@@ -0,0 +1,65 @@
+// learning about and playing with copy constructors!
+// http://www.geeksforgeeks.org/copy-constructor-in-cpp/
+
+#include<iostream>
+#include<cstring> // enables strlen(), strcpy(), among others I'm sure
+
+class String {
+	private: 
+		char * s;
+		int size;
+	public: 
+		String(const char * str = NULL); // constructor
+		~String() { delete [] s; } // destructor
+		// CE: why doesn't this need a proper variable to pass as arg after '&'?
+		String(const String & ); // copy constructor
+		void print() {std::cout << s << std::endl; } // function to print string
+		void change(const char * ); // function to change
+} ;
+
+// constructor:
+String::String(const char * str) {
+	size = strlen(str); // strlen() must be 
+	s = new char[size+1];
+	strcpy(s, str);
+}
+
+// function to change: 
+void String::change(const char * str) {
+	delete [] s;
+	size = strlen(str);
+	s = new char[size+1];
+	strcpy(s, str);
+}
+
+// copy constructor: 
+// CE: so & old_str is the whole class object, which includes the private variables * s and size, along with maybe the public functions (and public variables if there were any). 
+String::String(const String & old_str) { 
+	size = old_str.size;
+	s = new char[size + 1];
+	strcpy(s, old_str.s);
+}
+
+int main() {
+	String str1("GeeksQuiz");
+	String str2 = str1;
+
+	str1.print(); // what gets printed?
+	str2.print();
+
+	str2.change("GeeksforGeeks");
+
+	str1.print(); // what gets printed now?
+	str2.print();	
+
+/*
+ruby@rubyVM:~/Projects/ThinkLikeProgrammer/Ch5_exercises/sandbox$ g++ copy_constructor_02.cpp -o ../bin/copy_constructor_02
+ruby@rubyVM:~/Projects/ThinkLikeProgrammer/Ch5_exercises/sandbox$ ../bin/copy_constructor_02
+GeeksQuiz
+GeeksQuiz
+GeeksQuiz
+GeeksforGeeks
+
+*/
+	return 0;
+}
